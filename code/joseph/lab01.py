678da4d368bed29146de76473a4e51ee5df40ba2
Joseph Brown-Womack <josephbrownwomack@gmail.com>
2022-01-04 15:17:04 -0800

    Added lab01.py version 3

--- code/joseph/lab01.py
+++ code/joseph/lab01.py
@@ -4,5 +4,39 @@
 #print(feet, 'ft is equal to ', m, 'meters')
 # version 1
 # version 2
+#distance=float(input('What is the distance? '))
+#unit=input('What is the unit? (ft, m, mi, km) ' )
+#d_ft=distance*0.3048
+#d_m=distance
+#d_mi=distance*1609.34
+#d_km=distance*1000
+#if unit=='ft':
+#    print(distance, 'ft is ',d_ft ,'m')
+#if unit=='m':
+#    print(distance, 'm is ',distance ,'m')
+#if unit=='mi':
+#    print(distance, 'mi is ',d_mi ,'m')
+#if unit=='km':
+#    print(distance, 'km is ',d_km , 'm')
+#version 2
+#version 3
 distance=float(input('What is the distance? '))
-unit=input('What is the unit? (ft, m, mi, km) ' )
+unit=input('What is the unit? (ft, m, mi, km, yd, in) ' )
+d_ft=distance*0.3048
+d_m=distance
+d_mi=distance*1609.34
+d_km=distance*1000
+d_yd=distance*0.9144
+d_in=distance*0.0254
+if unit=='ft':
+    print(distance, 'ft is ',d_ft ,'m')
+if unit=='m':
+    print(distance, 'm is ',distance ,'m')
+if unit=='mi':
+    print(distance, 'mi is ',d_mi ,'m')
+if unit=='km':
+    print(distance, 'km is ',d_km ,'m')
+if unit=='yd':
+    print(distance, 'yd is ',d_yd ,'m')
+if unit=='in':
+    print(distance, 'in is ',d_in , 'm')
