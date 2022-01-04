1a9f5d9d5360a34a1f536c0c8607c7f3eeb100d5
Joseph Brown-Womack <josephbrownwomack@gmail.com>
2022-01-04 14:39:47 -0800

    Added lab01.py

---
+++ code/joseph/lab01.py
@@ -0,0 +1,8 @@
+# version 1
+#feet=float(input('How many feet? '))
+#m=feet*0.3048
+#print(feet, 'ft is equal to ', m, 'meters')
+# version 1
+# version 2
+distance=float(input('What is the distance? '))
+unit=input('What is the unit? (ft, m, mi, km) ' )
