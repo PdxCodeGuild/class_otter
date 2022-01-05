0a157a4917231afe32de937014c1dd90c823a1c6
Bruce Stull <bruce.stull@gmail.com>
2022-01-05 04:15:50 -0500

    Started lab02.py.

---
+++ code/bruce/lab02.py
@@ -0,0 +1,10 @@
+# *********************** #
+#   Author: Bruce Stull   #
+#     Average Numbers     #
+#       Version: 0.0      #
+#        2022-01-05       #
+# *********************** #
+
+# Assignment:
+# https://github.com/PdxCodeGuild/class_otter/blob/main/1%20Python/labs/02%20Average%20Numbers.md
+
