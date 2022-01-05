cd96aefe42e622cf04737b6bfac58f1f90d55b6e
Justin <justin.35f@gmail.com>
2022-01-05 11:53:41 -0800

    Lab 03 - Version 2

--- code/justin/Unit 01/Lab 03.py
+++ code/justin/Unit 01/Lab 03.py
@@ -5,7 +5,6 @@
 # Convert a given number into its english representation. For example: 67 becomes 'sixty-seven'. Handle numbers from 0-99.
 
 small_number_names = {
-    0: 'zero',
     1: 'one',
     2: 'two',
     3: 'three',
@@ -24,7 +23,7 @@ small_number_names = {
     16: 'sixteen',
     17: 'seventeen',
     18: 'eighteen',
-    19: 'nineteem' }
+    19: 'nineteen' }
 
 tens_place_names = {
     2: 'twenty',
@@ -37,10 +36,25 @@ tens_place_names = {
     9: 'ninety' }
 
 def convert_num_to_phrase(num):
+    if num == 0:
+        return 'zero'
+
     if num < 20:
         return small_number_names[num]
 
-    result = f"{tens_place_names[num // 10]}"
+    result = ''
+    if num > 99:
+        hundreds_digit = num // 100
+        result = f"{small_number_names[hundreds_digit]} hundred "
+        num -= hundreds_digit * 100
+
+    if num < 1:
+        return result[:-1]
+    elif num > 19:
+        result += f"{tens_place_names[num // 10]}"
+    else:
+        return result + f"{small_number_names[num]}"
+
     ones_digit = num % 10
 
     if ones_digit != 0:
@@ -56,3 +70,9 @@ def test_convert_num_to_phrase():
     assert convert_num_to_phrase(45) == 'forty-five'
     assert convert_num_to_phrase(67) == 'sixty-seven'
     assert convert_num_to_phrase(99) == 'ninety-nine'
+    assert convert_num_to_phrase(100) == 'one hundred'
+    assert convert_num_to_phrase(101) == 'one hundred one'
+    assert convert_num_to_phrase(337) == 'three hundred thirty-seven'
+    assert convert_num_to_phrase(619) == 'six hundred nineteen'
+    assert convert_num_to_phrase(811) == 'eight hundred eleven'
+    assert convert_num_to_phrase(999) == 'nine hundred ninety-nine'
