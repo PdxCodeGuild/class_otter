a4a73b0c4869ce7a3d7a9231cd4d98bf8585616e
Joseph Brown-Womack <josephbrownwomack@gmail.com>
2022-01-05 15:44:46 -0800

    lab02.py add versions 1 and 2

--- code/joseph/lab02.py
+++ code/joseph/lab02.py
@@ -1,19 +1,22 @@
 #Average Numbers
 #Version 1
-#nums = [5, 0, 8, 3, 4, 1, 6]
 
-# loop over the elements
+#nums = [5, 0, 8, 3, 4, 1, 6]
 #for num in nums:
 #    print(num)
-
-# loop over the indices
 #for i in range(len(nums)):
 #    print(nums[i])
-
-nums = [5, 0, 8, 3, 4, 1, 6]
-for num in nums:
-    print(num)
-for i in range(len(nums)):
-    print(nums[i])
+#average = sum(nums) / len(nums)
+#print('The average is ' + str(round(average, 2)))
+#version 1
+#version 2
+nums = []
+while True:
+    num = input('enter a number or done: ')
+    if num == 'done':
+        break
+    nums.append(int(num))
+print(nums)
 average = sum(nums) / len(nums)
 print('The average is ' + str(round(average, 2)))
+#version 2
