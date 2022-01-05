95147ed7446e09b87b04b98eb56bf5eb7ef65e13
Bruce Stull <bruce.stull@gmail.com>
2022-01-05 05:15:23 -0500

    lab02.py version 2.0.

--- code/bruce/lab02.py
+++ code/bruce/lab02.py
@@ -1,29 +1,57 @@
 # *********************** #
 #   Author: Bruce Stull   #
 #     Average Numbers     #
-#       Version: 1.0      #
+#       Version: 2.0      #
 #        2022-01-05       #
 # *********************** #
 
 # Assignment:
 # https://github.com/PdxCodeGuild/class_otter/blob/main/1%20Python/labs/02%20Average%20Numbers.md
 
-# Provided list:
-list_of_integers = [5, 0, 8, 3, 4, 1, 6]
+## Loop to prompt user to enter values to average. ##
+# Initialize the list.
+list_of_numbers = []
+# 'while' loop to prompt user for input.
+while True:
+    # Prompt user for input.
+    user_input_value_as_string = input("Please enter a number to add to the list to be averaged (type 'done' when completed): ")
+    
+    # If user input is 'done':
+    if user_input_value_as_string == 'done':
+        # Break out of loop and send to calculator.
+        break
+    
+    # If user input is not numeric:
+    # Can use 'elif' here.
+    if not user_input_value_as_string.isnumeric():
+        # Remind user of acceptable input values.
+        print("Please enter a numeric value or 'done'.")
+        # Go back to beginning of while loop.
+        continue
+    
+    # User input is not "done" and user input is numeric, so add the numeric value to the list.
+    list_of_numbers.append(float(user_input_value_as_string))
 
 ## Iterate over list and add the current list element to the running_sum. ##
 # Initialize variable:
 running_sum = 0
-# Define the 'for' loop, iterate over list_of_integers on each item.
+# Define the 'for' loop, iterate over list_of_numbers on each item.
 # We are using this loop to add up all the values of the items in the list:
-for item in list_of_integers:
+for item in list_of_numbers:
     # Add the current item value to running_sum:
     running_sum += item
 
-# Calculate the average, round the result to 2 decimal digits:
-average_of_list = round(running_sum / len(list_of_integers), 2)
+# Handle case where list_of_numbers is empty.
+if len(list_of_numbers) == 0:
+    result = "No numbers entered so there is no average value."
+# Otherwise, list contains some non-zero quantity of elements.
+else:
+    # Calculate the average, round the result to 2 decimal digits:
+    result = round(running_sum / len(list_of_numbers), 2)
 
 # Display the average:
 print(f'''
-The average of {list_of_integers} is: {average_of_list}.
+The list of numbers: {list_of_numbers}
+
+The average: {result}.
 ''')
