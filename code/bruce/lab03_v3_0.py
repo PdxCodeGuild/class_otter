b03804031efa47917beb7391e26516810c50e2b4
Bruce Stull <bruce.stull@gmail.com>
2022-01-05 15:03:44 -0500

    Edited comments for lab03_v3_0.py.

--- code/bruce/lab03_v3_0.py
+++ code/bruce/lab03_v3_0.py
@@ -14,10 +14,6 @@
 # Calculator:
 # https://www.calculatorsoup.com/calculators/conversions/roman-numeral-converter.php
 
-# Examples:
-# MMMM      :   4000
-# MMMCMXCIX :   3999
-
 # Use 'while' loop to prompt user and verify input is usable.
 while True:
     # Prompt user for input number.
@@ -36,29 +32,19 @@ Input string: {input_number_as_string} {type(input_number_as_string)}
 Input integer: {input_number_as_integer} {type(input_number_as_integer)}
 ''')
 
+# TODO: Create version where D, L, and V are calculated like the the M, C, X, and I.
+
 # Determine how many thousands (M).
 number_of_thousands = input_number_as_integer // 1000
 remainder = input_number_as_integer % 1000
 
-# # Determine how many five_hundreds (D).
-# number_of_five_hundreds = remainder // 500
-# remainder = remainder % 500
-
-# Determine how many hundreds (C).
+# Determine how many hundreds (C), which might be combined with D, M, or X.
 number_of_hundreds = remainder // 100
 remainder = remainder % 100
 
-# # Determine how many fifties (L).
-# number_of_fifties = remainder // 50
-# remainder = remainder % 50
-
-# Determine how many tens (X).
+# Determine how many tens (X), which might be combined with C, L or V.
 number_of_tens = remainder // 10
 remainder = remainder % 10
-
-# # Determine how many fives (V).
-# number_of_fives = remainder // 5
-# remainder = remainder % 5
 
 # Determine how many ones (I).
 number_of_ones = remainder
@@ -67,17 +53,14 @@ number_of_ones = remainder
 print(f'''
 Decimal:        {input_number_as_integer}
 Thousands:      {number_of_thousands}
-Five_Hundreds:  {'number_of_five_hundreds'}
 Hundreds:       {number_of_hundreds}
-Fifties:        {'number_of_fifties'}
 Tens:           {number_of_tens}
-Fives:          {'number_of_fives'}
 Ones:           {number_of_ones}
 ''')
 
 result = ''
 
-# Add Thousands.
+# Concatenate Thousands.
 if number_of_thousands == 4:
     result += "MMMM"
 elif number_of_thousands == 3:
@@ -87,11 +70,7 @@ elif number_of_thousands == 2:
 elif number_of_thousands == 1:
     result += "M"
 
-# # Add Five_Hundreds.
-# if number_of_five_hundreds == 1:
-#     result += "D"
-
-# Add Hundreds.
+# Concatenate Hundreds.
 if number_of_hundreds == 9:
     result += "CM"
 elif number_of_hundreds == 8:
@@ -111,11 +90,7 @@ elif number_of_hundreds == 2:
 elif number_of_hundreds == 1:
     result += "C"
 
-# # Add Fifties.
-# if number_of_fifties == 1:
-#     result += "L"
-
-# Add Tens.
+# Concatenate Tens.
 if number_of_tens == 9:
     result += "XC"
 elif number_of_tens == 8:
@@ -135,11 +110,7 @@ elif number_of_tens == 2:
 elif number_of_tens == 1:
     result += "X"
 
-# # Add Fives.
-# if number_of_fives == 1:
-#     result += "V"
-
-# Add Ones.
+# Concatenate Ones.
 if number_of_ones == 9:
     result += "IX"
 elif number_of_ones == 8:
