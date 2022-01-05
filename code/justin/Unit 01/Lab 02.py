d8a423347e1181fc18e7d0c6f26f286226a814e1
Justin <justin.35f@gmail.com>
2022-01-05 10:49:37 -0800

    Lab 02 - Average Numbers

--- code/justin/Unit 01/Lab 02.py
+++ code/justin/Unit 01/Lab 02.py
@@ -6,7 +6,7 @@
 # then divide that sum by the number of elements in that list. Remember len will give you the length of a list.
 # nums = [5, 0, 8, 3, 4, 1, 6]
 
-def average_list_v1(list):
+def average_list(list):
     sum = 0
 
     for item in list:
@@ -14,5 +14,36 @@ def average_list_v1(list):
 
     return sum / len(list)
 
-def test_average_list_v1():
-    assert average_list_v1([5, 0, 8, 3, 4, 1, 6]) == 3.857142857142857
+def test_average_list():
+    assert average_list([5, 0, 8, 3, 4, 1, 6]) == 3.857142857142857
+
+
+
+def is_done(response):
+
+    return str(response).lower() == 'done'
+
+def test_is_done():
+    assert is_done(5) == False
+    assert is_done('d') == False
+    assert is_done('done') == True
+    assert is_done('DoNe') == True
+    assert is_done('DONE') == True
+
+def get_list_from_user():
+    result = []
+    response = ""
+    while not is_done(response):
+        if len(response) != 0:
+            result.append(int(response))
+        response = input("Enter a number, or [done]: ")
+
+    return result
+
+def display_list_average():
+    list_to_average = get_list_from_user()
+    average = average_list(list_to_average)
+
+    print(average)
+
+display_list_average()
