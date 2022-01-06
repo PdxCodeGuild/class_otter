b3ed12672152ebd621878255aec5c7afa094f454
Sana Rahmani <sanarahmani@live.com>
2022-01-06 15:59:41 -0600

    Blackjack & Pick6

---
+++ code/sana/Pick6.py
@@ -0,0 +1,38 @@
+import random
+matches = {
+    1 : 4,
+    2 : 7,
+    3 : 100,
+    4 : 50000,
+    5 : 1000000,
+    6 : 25000000
+}
+balance = 0
+earnings = 0
+expenses = 0
+for tickets in range(1000):
+    winning_ticket = []
+    generated_ticket = []
+    for x in range(6):
+        win_nums = random.randint(1, 99)
+    # print(win_nums)
+        winning_ticket.append(win_nums)
+    print(winning_ticket)
+    for x in range(6):
+        gen_nums = random.randint(1, 99)
+    # print(gen_nums)
+        generated_ticket.append(gen_nums)
+    print(generated_ticket)
+    counter = 0
+    for x in range(len(winning_ticket)):
+        if winning_ticket[x] == generated_ticket[x]:
+            counter = counter + 1
+            print(counter)
+            x = x + 1
+    expenses = expenses + 2
+    print(expenses)
+    if counter in matches:
+        earnings = earnings + matches[counter]
+        print(earnings)
+    balance = earnings - expenses
+print(f"This is your ROI is ${balance}")
