1ec5ca535a3ba16d8120cbc34016acf85c71f285
Nick Corner <nickcorner421@gmail.com>
2022-01-06 11:26:15 -0600

    lab02 v1 and v2

---
+++ code/nick/python/lab02averageNumbers/lab02v1.py
@@ -0,0 +1,21 @@
+nums = [5, 0, 8, 3, 4, 1, 6]
+
+sum_of_numbers = sum(nums)
+
+amount_of_numbers = len(nums)
+
+print()
+
+average = sum_of_numbers/amount_of_numbers
+
+print(f'average {average}')
+
+print()
+
+for num in nums:
+    print(num)
+
+print()
+
+for num in range(len(nums)):
+    print(num)
