3a009ac638827bf0c127b8fae8ac5746fbe03651
Josse <perez.josse@hotmail.com>
2022-01-06 11:25:28 -0500

    new changes

--- code/josse/lab01.py
+++ code/josse/lab01.py
@@ -1,4 +1,4 @@
-user = float(input("what is distance? "))
+'''user = float(input("what is distance? "))
 units = input("please choose feet,meters, miles,kilometers,yards, inches: ")
 feet = 0.3048
 miles = 1609.34
@@ -29,8 +29,24 @@ elif units == "inches":
     output = user * inch
     print(output)
 
-print(output)
+print(output)'''
 
+metrics = {
+    
+"feet" : 0.3048,
+"miles" : 1609.34,
+"kilometers" : 1000,
+"yard" : 0.9144,
+"inch" : 0.0254,
 
+}
 
+distance = float(input("what is your distance? "))
+input_1 = input("pick input unit feet,miles,kilometers,yard or inch? ")
+output_1 = input("pick output unit feet,miles,kilometers,yard or inch? ")
 
+user = distance * metrics[input_1]
+
+answer = user / metrics[output_1]
+
+print(f"your conversion is {answer} {output_1}")
---
+++ code/josse/lab02.py
@@ -0,0 +1,32 @@
+'''nums = [5, 0, 8, 3, 4, 1, 6]
+
+for num in nums:
+   print(num)
+
+counter = 0
+
+for i in range(len(nums)):
+    counter += nums[i]
+
+average = counter / nums[i]
+
+print(average)'''
+
+#----------------------------------------------------------------------#
+
+'''if number == nums:
+    print(number)
+
+elif number == "done":
+    print("done")'''
+
+
+nums = []
+
+number = input("please enter a number or done:  ") 
+
+nums.append(float(number))
+
+
+print(nums)
+
