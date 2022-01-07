156b4b4979efdd4f4da531aa446412cf9f48aed2
Josse <perez.josse@hotmail.com>
2022-01-06 23:14:38 -0500

    practice

---
+++ code/josse/lab03.py
@@ -0,0 +1,16 @@
+
+
+#convert = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+convert = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+for word in convert:
+     if user == 7:
+        print(convert[0])
+
+
+user = input("please enter a number: ")
+
+
