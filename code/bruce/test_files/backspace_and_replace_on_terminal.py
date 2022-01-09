e690412385093e8852c9d616ef02f50d43c36ae8
Bruce Stull <bruce.stull@gmail.com>
2022-01-09 09:57:02 -0500

    Moved non-finished optional lab to test_files. Should have just kept that item on working_branch. Renamed backspace learning file.

--- code/bruce/test_files/backspace_and_replace_on_terminal.py
+++ code/bruce/test_files/backspace_and_replace_on_terminal.py
@@ -6,14 +6,10 @@
 #                   2022-01-09                  #
 # ********************************************* #
 
+# Import 'time' to use 'sleep()' later.
 import time
+# Import 'random' to use 'randint()' later.
 import random
-
-# # Print a string with "end=''".
-# print('first string', end='')
-
-# # Print a string with '\b'.
-# print('\bnext string')
 
 def generate_random_time_delay(time = .5, low = 0, hi = 10):
     '''Generates a random value between 0/10 and 10/10 of time.'''
@@ -36,24 +32,24 @@ def generate_random_index(string = ''):
     # return random.choice(range(len(string)))
 
 def test_generate_random_index():
+    # Empty string argument should return 0.
     assert generate_random_index('') == 0
+    # Argument string of length 3 should return number between 0 inclusive and 2 inclusive.
     assert 0 <= generate_random_index('abc') <= 2
-    assert not 3 < generate_random_index('abc')
+    # generate_random_index('abc') should not be greater than or equal to 3.
+    assert not 3 <= generate_random_index('abc')
+    # generate_random_index('abc') should not be less than 0.
     assert not generate_random_index('abc') < 0
+    # generate_random_index('abcdef') should be between 0 inclusive and 5 inclusive.
     assert 0 <= generate_random_index('abcdef') <= 5
-    assert not 6 < generate_random_index('abcdef')
+    # generate_random_index('abcdef') should not be greater than or equal to 6 inclusive.
+    assert not 6 <= generate_random_index('abcdef')
 
 def main():
 
     # TODO: Break these operations out into functions.
-    # to see if we can print a '\b' and replace the letter with different character.
     print_string = "abcdefghijk"
-    # Use this when we want consistent time delay.
-    # time_delay = .25
-    # Create random int variable to be used for backspace position.
-    # random_i = random.randint(0, len(print_string))
-    # random_i = generate_random_index(print_string)
-    # Use for loop to put time delay in between printing characters
+    # Use for loop to put time delay in between printing characters.
     for i, character in enumerate(print_string):
         # Create random time delay value.
         # This can be used to simulate real typing.
@@ -61,23 +57,11 @@ def main():
         # NOTE: Need the "end='', flush=True".
         print(character, end='', flush=True)
         time.sleep(random_time_delay)
-        # Randomly print '\b' and '?'.
-        # The effect is to backspace and replace a character with '?'.
-        # This effect will not occur if current i is greater than generate_random_index(print_string).
+        # Loop to add a randomly-placed delete of character and replace with '?'.
         if i == generate_random_index(print_string):
             print('\b', end='', flush=True)
             time.sleep(random_time_delay)
             print('?', end='', flush=True)
             time.sleep(random_time_delay)
-    
-    # # This loop only moved the cursor and doesn't delete any characters.
-    # for character in "\b\b\b":
-    #     print(character, end='', flush=True)
-    #     time.sleep(.1)
-    
-    # # This loop replaces the character, at the current position, with a space.
-    # for character in "\b":
-    #     print(character, end=' ', flush=True)
-    #     time.sleep(.1)
 
 main()
