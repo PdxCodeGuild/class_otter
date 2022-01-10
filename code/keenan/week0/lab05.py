82712591036f8fe4fe3b2ec5b33287b7310ce65d
ktabusa laptop <ktabusa@gmail.com>
2022-01-10 09:31:05 -0800

    adding WIP lab 5

---
+++ code/keenan/week0/lab05.py
@@ -0,0 +1,57 @@
+# Lab 5: Pick6 Lottery Simulation
+# A ticket contains 6 numbers, 1 to 99, and the number of matches between the ticket and
+# the winning numbers determines the payoff. Order matters, if the winning numbers are 
+# [5, 10] and your ticket numbers are [10, 5] you have 0 matches. If the winning 
+# numbers are [5, 10, 2] and your ticket numbers are [10, 5, 2], you have 1 match. 
+# Calculate your net winnings (the sum of all expenses and earnings).
+
+
+import random
+
+ticket_cost = 2
+
+# dictionary containing the number of matches to the corresponding dollar amount of winnings
+winnings = {
+    0: 0,
+    1: 4,
+    2: 7,
+    3: 100,
+    4: 50000,
+    5: 1000000,
+    6: 25000000,
+    }
+
+# define a function that will generate a 5 member list with random nums 1-99
+def pick6():
+    list = [] 
+    for i in range(7):
+        num = random.randint(1,99)
+        list.append(num)
+    return list
+
+# define lists for the winning and ticket numbers
+winning_nums = pick6()
+ticket_nums = pick6()
+
+# print(winning_nums)
+# print(ticket_nums)
+
+# define a function to compare the winning numbers to the ticket numbers
+matches = 0
+def num_matches(winning, ticket):
+    matches = 0
+    for i in range(7):
+        if winning[i] == ticket[i]:
+            matches += 1
+    
+    return matches
+
+num_matches(winning_nums, ticket_nums)
+
+expenses = ticket_cost
+earnings = winnings[matches] - expenses
+print(f'{earnings} dollars earned.')
+
+
+# Version 2: The ROI is defined as (earnings - expenses)/expenses.
+# Calculate your ROI and print it out along with earnings and expenses.
