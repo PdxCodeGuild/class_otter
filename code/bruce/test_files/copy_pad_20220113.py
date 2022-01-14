07a9e1ea90130cf553d056a07d5c6b3e1d2926dd
Bruce Stull <bruce.stull@gmail.com>
2022-01-13 20:59:07 -0500

    Test

--- code/bruce/test_files/copy_pad_20220113.py
+++ code/bruce/test_files/copy_pad_20220113.py
@@ -6,24 +6,21 @@
 #        2022-01-13       #
 # *********************** #
 
-lines = []
-print(lines)
-print(len(lines))
-# lines[0]
-
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-thisdict["color"] = "red"
 
 dict_01 = {'a':'1','b':'2'}
 dict_02 = {'b':'2','a':'1'}
 
-print(dict_01 == dict_02)
+list_to_save = [dict_01,dict_02]
 
-print(len(dict_02))
+print(list_to_save)
+
+# Convert list of dictionaries to a comma-separated string, with '\n' between dictionaries.
+
+def dict_to_string(dictionary):
+    '''Accepts a dictionary argument. Converts the dictionary to csv string. Returns the string.'''
+    return_string = ''
+    for key in dictionary.keys():
+        print(key)
 
 
+dict_to_string(dict_01)
