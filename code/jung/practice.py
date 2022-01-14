4a2d1f22ef899ecaea7e6cbe8302f9eed8a3e4f2
Jung <nurunggee@gmail.com>
2022-01-14 13:45:17 -0800

    lab10

--- code/jung/practice.py
+++ code/jung/practice.py
@@ -17,3 +17,15 @@ def test_ones_digit():
     assert ones_digit(3) == 3
     assert ones_digit(56) == 6
     assert ones_digit(812) == 2
+
+
+
+def percentage(v, max):
+    p = v / max * 100
+    p = int(p)
+    return f"{p}%"
+
+def test_precentage():
+    assert percentage(1, 10) == '10%'
+    assert percentage(600, 1200) == '50%'
+    assert percentage(1, 3) == '33%'
