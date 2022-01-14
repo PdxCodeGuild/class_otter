7bf3b936c4dab02fe7989559f167f6cae5034d88
Josse <perez.josse@hotmail.com>
2022-01-14 17:37:42 -0500

    lab 10 work in progress

--- code/josse/lab10test.py
+++ code/josse/lab10test.py
@@ -1,34 +1,77 @@
 with open('example.csv', 'r') as file:
     lines = file.read().split('\n')
-    # print(lines)
 
-new_list = []
 
-for row in lines:
-    new_list.append(row.split(","))
+def extract_string(input_lines):
+    list = []
+    for row in input_lines:
+        list.append(row.split(","))
+    return list
 
+
+new_list = extract_string(lines)
 key_list = new_list.pop(0)
 
-value_list = new_list
+
+def main_dict(header, values):
+    list_of_dicts = []
+    for value in values:
+        list_of_dicts.append(
+            {header[0]: value[0], header[1]: value[1], header[2]: value[2]})
+    return list_of_dicts
 
 
-def convert_to_dict(a):
-    dict_key = {}
-    for keys in dict(key_list):
-        dict_key[key_list]
-        return dict_key
+contacts = main_dict(key_list, new_list)
 
-
-print(convert_to_dict(key_list))
 # name = input("enter your name: ")
 # fav_color = input("enter your favorite color: ")
 # fav_fruit = input("enter your favorite fruit: ")
+
+# def create(user, answer):
+#     added_list = []
+#     for answer in answer:
+#         added_list.append(
+#             {user: name, user: fav_color, user: fav_fruit})
+#     return added_list
+
+print(contacts)
+
+
+# new_dict = {}
+# for i in range(len(header)):
+#     new_dict[header[i]]
+
+
+# def convert(conversion):
+#     dict_header = []
+#     for string in conversion:
+#         dict_header.join(string)
+#     return dict_header
+
+# def convert(list_converter):
+#      dict_header = {}
+#     for dictionary in list_converter:
+#         dict_header.append(dictionary.split(" , "))
+#     return dict_header
+
+
+# new_list = []
+# for row in lines:
+#     new_list.append(row.split(","))
+
+# value_list = new_list
+
+# def convert_to_dict(a):
+#     dict_key = {}
+#     for keys in dict(key_list):
+#         dict_key[key_list]
+#         return dict_key
+
 
 # if "'name,favorite fruit,favorite color'" in lines:
 #     print(True)
 # elif "'name,favorite fruit,favorite color'" != lines:
 #     print(False)
-
 
 # def convert():
 
