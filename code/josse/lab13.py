e40e55e32e83e2e138b0e739fec4692b29bd5592
Josse <perez.josse@hotmail.com>
2022-01-19 18:28:42 -0500

    lab 13 w.i.p

---
+++ code/josse/lab13.py
@@ -0,0 +1,40 @@
+class Player:
+    def __init__(self):
+        self.name = input("please enter your name: ")
+        self.token = "X" or "O"
+
+
+class Game:
+    board_input = []
+
+    def __repr__():
+
+    def move(x, y, player):
+
+    def calc_winner():
+
+    def is_full():
+
+    def is_game_over():
+
+
+def main():
+    user_input = input(f"please enter your first move: ")
+    if user_input == self.token:
+        print("X")
+
+
+# player = Player()
+
+# while True:
+#     user_input = input("please enter your move:")
+#     if user_input == 'q':
+#         break
+#     elif user_input == 'c':
+#         create_contact(data, keys)
+#     elif user_input == 'r':
+#         read_contact(data, keys)
+#     elif user_input == 'u':
+#         update_contact(data, keys)
+#     elif user_input == 'd':
+#         delete_contact(data, keys)
