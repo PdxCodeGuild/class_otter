19d12e466344c25f454076ec4335b5351bbdd071
Bruce Stull <bruce.stull@gmail.com>
2022-01-20 16:43:09 -0500

    lab13.py Version 1.0 Completed. Needs further testing for reliability.

--- code/bruce/labs/lab13.py
+++ code/bruce/labs/lab13.py
@@ -32,124 +32,109 @@ class Player:
     def __init__(self, name, token):
         self.name = name
         self.token = token  # Token will be either 'X' or 'O'.
-        # self.x = x
-        # self.y = y
 
     def __str__(self) -> str:
-        return f"{self.name}:{self.token}"
-    
-    # def get_position(self):
-    #     return self.x, self.y
+        return f"{self.name} : {self.token}"
 ####################################################
 
 ################### The Game ###################
 # TODO: Add functionality where size of board can be provided by a class variable.
 class Game:
     def __init__(self):
-        # self.board = board
+
         # # x:              0       1       2     0       1       2      0       1       2
         # # y:              0       0       0     1       1       1      2       2       2
-        # self.board = [['-','|','-','|','-'],['-', '|','-','|','-'],['-','|','-','|','-']]   # Do we need the '|'?
-
-        # x:            0   1   2     0   1   2     0   1   2
-        # y:            0   0   0     1   1   1     2   2   2
-        self.board = [['-','-','-'],['-','-','-'],['-','-','-']]    # This is without the '|'. The '|' can be added at end in a '|'.join().
-        
-        # TODO: This will be used in future expansions. Keep a history of the game so it can be replayed.
-        # Store the Player.name, Player.token, Player.x, and Player.y as a list within a list.
-        self.move_history = []
-        
-        # V 1.0
-        # self.board_as_string = f"{'|'.join([column for column in self.board[0]])}\n{'|'.join([column for column in self.board[1]])}\n{'|'.join([column for column in self.board[1]])}"
-        # V 1.1 TODO: Can we compact this?
-        # self.board_as_string = f"{'|'.join([column for column in self.board[0]])}\n{'|'.join([column for column in self.board[1]])}\n{'|'.join([column for column in self.board[1]])}"
-
-        # self.player1position = [x,y]
-        # self.player2position = [x,y]
-        # Do we need to instantiate player positions on game start?
-        # No, we do not want to do that, it wouldn't make sense, since instantiating requires some value.
-        # We can probably just grab position from Player?
-        
-        ########################################## NOTE!!! ##########################################
-        # Player positions are kept track of in self.board
-        #############################################################################################
-        # self.player_positions = [[],[]] <<== Not needed!!!
+        # self.__board = [['-','|','-','|','-'],['-', '|','-','|','-'],['-','|','-','|','-']]   # Do we need the '|'?
 
         # NOTE: Use something like:
         # [['O','-','-'],['-','X','-'],['-','-','-']]
 
+        # x:            0   1   2     0   1   2     0   1   2
+        # y:            0   0   0     1   1   1     2   2   2
+        self.__board = [['-','-','-'],['-','-','-'],['-','-','-']]    # This is without the '|'. The '|' can be added at end in a '|'.join().
+        
+        # TODO: This move_history will be used in future expansions. Keep a history of the game so it can be replayed.
+        # Store the Player.name, Player.token, Player.x, and Player.y as a list within a list.
+        self.move_history = []
+        
+        ########################################## NOTE!!! ##########################################
+        # Player positions are kept track of in self.__board
+        # self.player_positions = [[],[]] <<== Not needed!!!
+        #############################################################################################
+
     def __str__(self) -> str:
         '''Returns a representation of the board status at a current point in the game.'''
-        # return f"{self.board_as_string}"
-        # Can use loop to print each line individually.
         
         # Join the sub-lists by '|'.
-        row_strings = ['|'.join(sub_list) for sub_list in self.board]
+        row_strings = ['|'.join(sub_list) for sub_list in self.__board]
         # Join the lists by '\n'
         result_string = '\n'.join(row_strings)
-        # return f"{'|'.join([column for column in self.board[0]])}\n{'|'.join([column for column in self.board[1]])}\n{'|'.join([column for column in self.board[2]])}"
+        # return f"{'|'.join([column for column in self.__board[0]])}\n{'|'.join([column for column in self.__board[1]])}\n{'|'.join([column for column in self.__board[2]])}"
         return result_string
 
-    def move(self, x, y, token):
+    # TODO: Add functionality to log game moves to move_history.
+    def move(self, x, y, player):
         '''Checks if position x,y is available. If so, place the token for one of the players at x,y.'''
         if self.position_available(x, y):
-            self.board[y][x] = token
-            # This will later be changed to an entry in a list for future playback.
-            # return f"{token} placed at: {x},{y}"
+            self.__board[y][x] = player.token
             return True
-        # This will later be changed to an entry in a list for future playback.
-        # return f"{token} not placed at: {x},{y}"
         return False
-    
+
     def position_available(self, x, y):
-        '''Check if position is available for play. Returns True if available, A string is returned otherwise.'''
-        if self.board[y][x] == '-': 
+        '''Check if position is available for play. Returns True if available, False is returned otherwise.'''
+        if self.__board[y][x] == '-': 
             return True
         return False
     
-    def calc_winner(self):
+    def calc_winner(self, player):
         '''Returns which token ('X' or 'O') has won, otherwise returns None.'''
 
         def transpose_list_of_lists(list_of_lists = [[],[]]):
-            '''Accepts an argument of a list of lists. Returns a transposed version of the list. List has to be 'rectangular' (sub-lists are all same lenght) to work.'''
+            '''Accepts an argument of a list of lists. Returns a transposed version of the list. List has to be 'rectangular' (sub-lists are all same length) to work.'''
             trasposed_list_of_lists = [[(list_of_lists[i][j]) for i in range(len(list_of_lists))] for j in range(len(list_of_lists[0]))]
             return trasposed_list_of_lists
         
         # TODO: Add functionality where these 'token's can be referred by variable so that players can choose their own unique token.
         # i.e. p1.token = 'B'
-        def row_win(board = self.board):
+        def row_win(board = self.__board):
             '''Returns player token for a row win, otherwise returns None.'''
             for row in board:
-                if row == ['X','X','X']:
-                    return 'X'
-                elif row == ['O','O','O']:
-                    return 'O'
+                if row[0] == row[1] == row[2] == player.token:
+                    return player.token
+                # if row == ['X','X','X']:
+                #     return 'X'
+                # elif row == ['O','O','O']:
+                #     return 'O'
             return None
         
         def column_win():
             '''Returns player token for a column win, otherwise returns None'''
-            board = transpose_list_of_lists(self.board)
+            board = transpose_list_of_lists(self.__board)
             result = row_win(board)
             return result
         
         def diagonal_win():
             '''Returns player token for diagonal win, otherwise returns None.'''
-            check_list = [self.board[i][i] for i in range(len(self.board))]
-            if check_list == ['X','X','X']:
-                return 'X'
-            elif check_list == ['O','O','O']:
-                return 'O'
+            check_list = [self.__board[i][i] for i in range(len(self.__board))]
+            if check_list[0] == check_list[1] == check_list[2] == player.token:
+                return player.token
+            # if check_list == ['X','X','X']:
+            #     return 'X'
+            # elif check_list == ['O','O','O']:
+            #     return 'O'
             return None
 
         def diagonal_cross_win():
             '''Returns player token for cross diagonal win, otherwise returns None.'''
             # Can we use something similar to the comprehension from diagonal win?
-            # check_list = [self.board[i][i] for i in range(len(self.board))]
-            check_list = [self.board[0][2],self.board[1][1],self.board[2][0]]
-            if check_list == ['X','X','X']:
-                return 'X'
-            elif check_list == ['O','O','O']:
-                return 'O'            
+            # check_list = [self.__board[i][i] for i in range(len(self.__board))]
+            check_list = [self.__board[0][2],self.__board[1][1],self.__board[2][0]]
+            if check_list[0] == check_list[1] == check_list[2] == player.token:
+                return player.token
+            # if check_list == ['X','X','X']:
+            #     return 'X'
+            # elif check_list == ['O','O','O']:
+            #     return 'O'            
             return None
 
         # Check for wins.
@@ -176,14 +161,16 @@ class Game:
 
     def is_full(self):
         '''Returns True if gameboard is full. Otherwise returns False.'''
-        # Is board full?
-        if '-' not in self.board:
-            return True
-        return False
-    
-    def is_game_over(self):
+        for row in self.__board:
+            for column in row:
+                # Game is not full if there are any remaining '-' on the board.
+                if column == '-':
+                    return False
+        return True
+
+    def is_game_over(self, player):
         '''Returns True if game board is full or a player has won.'''
-        if self.is_full() or self.calc_winner() != None:
+        if self.is_full() or self.calc_winner(player) != None:
             return True
         return False
     
@@ -196,20 +183,86 @@ def test_position_available():
     assert g1.position_available(0, 0) == True
 
     g2 = Game()
-    g2.board[0][0] = 'X'
+    g2._Game__board[0][0] = 'X'
     assert g2.position_available(0, 0) == False
 
     g3 = Game()
-    g3.board[0][0] = 'O'
+    g3._Game__board[0][0] = 'O'
     assert g3.position_available(0, 0) == False
 
 def test_move():
     g1 = Game()
-    g1.board[1][1] = 'X'
-    assert g1.move(1, 1, 'O') == "O not placed at: 1,1"
+    p1 = Player('Cornelius','C')
+
+    g1._Game__board[1][1] = 'X'
+    assert g1.move(1, 1, p1) == False
 
     g2 = Game()
-    assert g2.move(1, 1, 'X') == "X placed at: 1,1"
+    assert g2.move(1, 1, p1) == True
+
+def test_is_full():
+    g1 = Game()
+    g1._Game__board = [['X','X','X'],['X','X','X'],['X','X','X']]
+    assert g1.is_full() == True
+
+    g2 = Game()
+    g2._Game__board = [['O','O','O'],['O','O','O'],['O','O','O']]
+    assert g2.is_full() == True
+
+    g3 = Game()
+    g3._Game__board = [['-','-','-'],['-','-','-'],['-','-','-']]
+    assert g3.is_full() == False
+
+    g4 = Game()
+    g4._Game__board = [['X','X','X'],['X','X','-'],['X','X','X']]
+    assert g4.is_full() == False
+
+    g5 = Game()
+    g5._Game__board = [['X','-','X'],['X','X','X'],['X','X','X']]
+    assert g5.is_full() == False
+
+    g6 = Game()
+    g6._Game__board = [['X','X','X'],['X','X','X'],['X','X','-']]
+    assert g6.is_full() == False
+
+def test_calc_winner():
+    g1 = Game()
+    p1 = Player('JakeBrake','X')
+    g1._Game__board = [['X','X','X'],['-','-','-'],['-','-','-']]
+    assert g1.calc_winner(p1) == 'X'
+
+    g2 = Game()
+    p2 = Player('Olander','O')
+    g2._Game__board = [['O','O','O'],['-','-','-'],['-','-','-']]
+    assert g2.calc_winner(p2) == 'O'
+
+    g3 = Game()
+    p3 = Player('Cobb','O')
+    g3._Game__board = [['O','-','-'],['O','-','-'],['O','-','-']]
+    assert g3.calc_winner(p3) == 'O'
+
+    g4 = Game()
+    p4 = Player('Yennifer','O')
+    g4._Game__board = [['O','-','-'],['-','O','-'],['-','-','O']]
+    assert g4.calc_winner(p4) == 'O'
+
+    g5 = Game()
+    p5 = Player('Petra','O')
+    g5._Game__board = [['-','-','O'],['-','O','-'],['O','-','-']]
+    assert g5.calc_winner(p5) == 'O'
+
+def test_is_game_over():
+    g1 = Game()
+    p1 = Player('Cavill','X')
+    assert g1.is_game_over(p1) == False
+
+    g2 = Game()
+    g2._Game__board = [['O','-','-'],['-','O','-'],['-','-','O']]
+    assert g2.is_game_over(p1) == False
+
+    g3 = Game()
+    g3._Game__board = [['X','X','X'],['X','X','X'],['X','X','X']]
+    assert g3.is_game_over(p1) == True
 
 ###############################################
 
@@ -224,113 +277,84 @@ def main():
     g1.print_player_info(p1)
     g1.print_player_info(p2)
     
-    # Set whos turn to player one.
-    players = [p1, p2]
-    players_turn = 0
+    players = {p1.name: p1, p2.name: p2}
+    # Set first player turn to player one.
+    # TODO: Add functionality where a random generator picks who goes first.
+    players_turn = p1.name
 
     print(g1)
-    print()
 
     while True:
 
-        # Manual moves.
-        # g1.move(0,2,'O')
-        # g1.move(1,1,'O')
-        # g1.move(2,0,'O')
-
-        # g1.move(0,0,'O')
-        # g1.move(1,1,'O')
-        # g1.move(2,2,'O')
-
-        # g1.move(0,0,'X')
-        # g1.move(1,0,'X')
-        # g1.move(2,0,'X')
-
-        # g1.move(0,0,'O')
-        # g1.move(0,1,'O')
-        # g1.move(0,2,'O')
-
-        # No winner.
-        # g1.move(0,0,'O')
-        # g1.move(0,1,'X')
-        # g1.move(0,2,'O')
-
-        # ###### Prompt for input and attempt move ######
-        # while True:
-        #     # Prompt player n for their move.
-        #     print(f"{players[players_turn].token} : {players[players_turn].name}'s turn!")
-        #     y_input = input(f"Choose your row: ")
-        #     x_input = input(f"Choose your column: ")
-        #     # If not valid, go back and re-prompt.
-        #     if not x_input.isnumeric() or not y_input.isnumeric():
-        #         print("Please enter numeric values between 0 and 2.")
-        #         continue
-        #     desired_x_position = int(x_input)
-        #     desired_y_position = int(y_input)
-        #     break
+        ###### Prompt for input and attempt move ######
+        while True:
+            # Prompt player n for their move.
+            print()
+            print(f"{players[players_turn].token} : {players[players_turn].name}'s turn!")
+            y_input = input(f"Choose your row: ")
+            x_input = input(f"Choose your column: ")
+            # If not valid, go back and re-prompt.
+            if not x_input.isnumeric() or not y_input.isnumeric():
+                print("Please enter numeric values between and including 1 and 3.")
+                continue
+            desired_x_position = int(x_input)
+            desired_y_position = int(y_input)
+            break
             
-        # # Move if available.
-        # if not g1.move(desired_x_position - 1, desired_y_position - 1, players[players_turn].token):
-        #     continue
-        # ###############################################
+        # Move if available.
+        if not g1.move(desired_x_position - 1, desired_y_position - 1, players[players_turn]):
+            continue
+        ###############################################
 
-        # ############ Check for winner ############
-        # print(f"g1.calc_winner(): {g1.calc_winner()}")
-        # calc_winner_result = g1.calc_winner()
-        # if calc_winner_result:
-        #     print(f"g1.calc_winner(): {calc_winner_result}")
-        # ##########################################
-        
-        ############### Test is_full() ###############
         print(g1)
 
-        g1.board = [['X','X','X'],['X','X','X'],['X','X','X']]
-        print(g1)
-        print(f"g1.is_full(): {g1.is_full()}")
+        # Check for win:
+            # If win:
+                # Announce winner
+                # End game
+            # Else:
+                # Proceed
+        ############ Check for winner ############
+        # print(f"g1.calc_winner(): {g1.calc_winner(players[players_turn])}")
+        calc_winner_result = g1.calc_winner(players[players_turn])
+        if calc_winner_result:
+            print(f"WINNER: {players[players_turn].token} : {players[players_turn].name}!!!")
+            break
+        ##########################################
 
-        g1.board = [['X','X','O'],['X','X','X'],['X','X','X']]
-        print(g1)
-        print(f"g1.is_full(): {g1.is_full()}")
+        # Check for full board:
+            # If full:
+                # Announce game is 'draw' or 'cat'
+                # End game
+            # Else:
+                # Proceed
+        ##########################################
+        if g1.is_full():
+            print("Game is a draw/cat.")
+            break
+        ##########################################
 
-        g1.board = [['X','-','O'],['X','X','X'],['X','X','X']]
-        print(g1)
-        print(f"g1.is_full(): {g1.is_full()}")
+        # Check is_game_over:
+            # If over:
+                # Announce game is over
+                # End game.
+            # Else:
+                # Proceed
+        ##########################################
+        if g1.is_game_over(players[players_turn]):
+            print("Game over!")
+            break
+        ##########################################
 
-        break
-        ##############################################
-
-
-        
+        # Toggle player turn
+        ##########################################
         # Toggle player turn to other player.
-        if players_turn == 0:
-            players_turn = 1
+        if players_turn == p1.name:
+            players_turn = p2.name
+        # elif players_turn == p2.name:
+        #     players_turn = p1.name
         else:
-            players_turn = 0
+            players_turn = p1.name
+        ##########################################
 
-
-
-    # p1 = Player("Earl",'X')
-    # p2 = Player("Dezzi",'O')
-    # print(p1)
-    # print(p2)
-
-    # g1 = Game()
-    # # print(g1)
-    # # g1.board[0][0] = 'X'
-    # # print(g1)
-    
-    # print(g1)
-    # # print(f"g1.position_available(p1.token, 0, 0): {g1.position_available(p1.token, 0, 0)}")
-    # # print(f"g1.move('X',x,y): {g1.move('X', 0, 0)}")
-    # print(g1.move('X', 0, 0))
-    # print(g1.move('X', 0, 0))
-    # print(g1.move('X', 1, 1))
-
-    # # print(f"g1.position_available(p1.token, 0, 0): {g1.position_available(p1.token, 0, 0)}")
-    # # print(f"g1.move('X',x,y): {g1.move('X', 0, 0)}")
-    # # print(f"g1.move('X',x,y): {g1.move('X', 1, 1)}")
-    # print(g1)
-
-
-
-# main()
+main()
