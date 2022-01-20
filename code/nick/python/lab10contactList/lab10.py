6f80a8d0787af808c4d984d293c589fd7b6455d2
Nick Corner <nickcorner421@gmail.com>
2022-01-19 18:35:04 -0600

    lab08 and lab10 updated

--- code/nick/python/lab10contactList/lab10.py
+++ code/nick/python/lab10contactList/lab10.py
@@ -13,37 +13,37 @@ with open('test.csv') as file:
 
 # Create a record: ask the user for each attribute, add a new contact to your contact list with the attributes that the user entered.
 
-name, fruit, color = input('enter name, favorite fruit and favorite color for new contact seperated by a space. ').split(' ')
 
-def create_new_contact(name, fruit, color):
+def create_new_contact():
+    name, fruit, color = input('enter name, favorite fruit and favorite color for new contact seperated by a space. ').split(' ')
     new_contact = {'name': name ,'favorite fruit': fruit,'favorite color': color}
     list_of_contacts.append(new_contact)
     print(new_contact)
-create_new_contact(name,fruit,color)
 
 
-retrieve_name = input('enter a name to retrieve contact info. ')
+
 
 # # Retrieve a record: ask the user for the contact's name, find the user with the given name, and display their information
 
-def retrieve_contact(key):
+def retrieve_contact():
+    retrieve_name = input('enter a name to retrieve contact info. ')
     for i in range(len(list_of_contacts)):
-        if key in list_of_contacts[i]['name']:
+        if retrieve_name in list_of_contacts[i]['name']:
             return print(f'your contact: {list_of_contacts[i]}')
-        elif key not in list_of_contacts[i]['name']:
+        elif retrieve_name not in list_of_contacts[i]['name']:
             pass
     print('not here')
 
-retrieve_contact(retrieve_name)
+
 
 # # Update a record: ask the user for the contact's name, 
 # # then for which attribute of the user they'd like to update and the value of the attribute they'd like to set.
 
-user_choice = input('enter the name of the contact you would like to update. ')
-atribute_to_change = input('name, favorite fruit, or favorite color? ')
 
 
-def update_contact(name,atribute):
+def update_contact():
+    name = input('enter the name of the contact you would like to update. ')
+    atribute = input('name, favorite fruit, or favorite color? ')
     name_selected = 0
     for i in range(len(list_of_contacts)):
         if name in list_of_contacts[i]['name']:
@@ -68,18 +68,48 @@ def update_contact(name,atribute):
         name_selected['favorite fruit'] = fruit
         print(name_selected)
 
-update_contact(user_choice,atribute_to_change)
+
 
 # Delete a record: ask the user for the contact's name, remove the contact with the given name from the contact list.
 
-delete_name = input('who would you like to remove from contacts? enter name. ')
 
-def remove_contact(name):
+def remove_contact():
+    name = input('who would you like to remove from contacts? enter name. ')
     for i in range(len(list_of_contacts)):
         if name in list_of_contacts[i]['name']:
             name_selected = list_of_contacts[i]
     print()
     list_of_contacts.remove(name_selected)
     print(list_of_contacts)
+# got this loop from merrit then inputed the functions i made.
+while True:
+    user_input = input("(c)reate, (r)read, (u)pdate, (d)elete, (q)uit?")
+    if user_input == 'q':
+        break
+    elif user_input == 'c':
+        create_new_contact()
+    elif user_input == 'r':
+        retrieve_contact()
+    elif user_input == 'u':
+        update_contact()
+    elif user_input == 'd':
+        remove_contact()
 
-remove_contact(delete_name)
+# gotta get the list of dictionaries into a list of lists
+
+dic_to_list_of_list = [[key for key in list_of_contacts[0].keys()], *[list(idx.values()) for idx in list_of_contacts]] 
+
+# then combined the lists of lists into one list.
+
+new_list = [','.join(i) for i in dic_to_list_of_list]
+
+# sepperating the list
+
+new_list = '\n'.join(new_list)
+
+# adding it to the csv file
+
+with open('test.csv', 'w') as file:
+    file.write(new_list)
+
+# for some reason i can open it in sheets or excel and see what ive added but when i open the csv in VS code i cant see them.
