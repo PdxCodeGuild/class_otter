a751ec2f439e1b51f611d361c16dd5c90f1a627e
Bruce Stull <bruce.stull@gmail.com>
2022-01-21 13:51:47 -0500

    Function practice files.

--- code/bruce/other_code/test_and_learning_files/two_simple_functions.py
+++ code/bruce/other_code/test_and_learning_files/two_simple_functions.py
@@ -7,5 +7,15 @@
 # Returns the the result of adding num_01 to num_02
 
 # Function named print_the_result.
-# Accepts argument, called input_argument, of any type.
+# Accepts argument of any type, name of argument should be input_argument.
 # Prints the value of the argument to console.
+
+def add_number(num_01, num_02):
+    answer = num_01 + num_02
+    print_the_result(f"result of addition: {answer}") # <<== Don't really need to make our own function since python alreacy includes print().
+    return answer
+
+def print_the_result(input_argument):
+    print(input_argument)
+
+number = add_number(3, 6)
