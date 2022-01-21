f09431691cc715f813ebddf005ce290cc898b602
Josse <perez.josse@hotmail.com>
2022-01-20 19:11:43 -0500

    updated labs

--- code/josse/lab13.py
+++ code/josse/lab13.py
@@ -1,40 +1,69 @@
 class Player:
-    def __init__(self):
-        self.name = input("please enter your name: ")
-        self.token = "X" or "O"
+    def __init__(self, name, token):
+        self.name = name
+        self.token = token
+
+    def __repr__(self):
+        return self.name
 
 
 class Game:
-    board_input = []
 
-    def __repr__():
+    def __init__(self):
+        self.board = [
 
-    def move(x, y, player):
+            [" ", " ", " "],
+            [" ", " ", " "],
+            [" ", " ", " "]
 
-    def calc_winner():
+        ]
 
-    def is_full():
+    def __repr__(self):
+        for line in self.board:
+            print("|".join(line))
 
-    def is_game_over():
+    def move(self, x, y, player):
+        self.board[x][y] = player.token
+
+    def calc_winner(self, player):
+        if self.board[0][0] == self.board[0][1] == self.board[0][2] == player.token:
+            return player.token
+        elif self.board[1][0] == self.board[1][1] == self.board[1][2] == player.token:
+            return player.token
+        elif self.board[2][0] == self.board[2][1] == self.board[2][2] == player.token:
+            return player.token
+        elif self.board[0][0] == self.board[1][1] == self.board[2][2] == player.token:
+            return player.token
+        elif self.board[0][2] == self.board[1][1] == self.board[2][0] == player.token:
+            return player.token
+        else:
+            return None
+
+    def is_full(self):
+
+    def is_game_over(self):
 
 
 def main():
-    user_input = input(f"please enter your first move: ")
-    if user_input == self.token:
-        print("X")
+    game = Game()
+
+    player_1_name = input(f"please enter your name: ")
+    player_2_name = input(f"please enter your name: ")
+
+    player_1_token = input(f" please select x or o: ").lower()
+    if player_1_token == "x":
+
+        player_2_token = "o"
+    else:
+        player_2_token = "x"
+
+    player_1 = Player(player_1_name, player_1_token)
+    player_2 = Player(player_2_name, player_2_token)
+
+    # print(player_1)
 
 
-# player = Player()
-
-# while True:
-#     user_input = input("please enter your move:")
-#     if user_input == 'q':
-#         break
-#     elif user_input == 'c':
-#         create_contact(data, keys)
-#     elif user_input == 'r':
-#         read_contact(data, keys)
-#     elif user_input == 'u':
-#         update_contact(data, keys)
-#     elif user_input == 'd':
-#         delete_contact(data, keys)
+# game.move(0, 0, player_1)
+# game.move(0, 1, player_2)
+# print(game.__repr__())
+main()
