fcbea28881c1d512d2e89417714bce2e0060ade9
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-01-26 10:30:44 -0800

    Lab group

---
+++ code/moss/jockos.py
@@ -0,0 +1,12 @@
+
+jacklopes = [0,0]
+
+year = 0
+
+while len(jacklopes) < 1001 :
+    year+= 1
+    
+    for i in range(len(jacklopes)):
+        if i < 10 :
+            jacklopes[i]+=1
+    for i  in range(len(jacklopes)):
