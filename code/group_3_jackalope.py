36bf63fabb41997999cbe9acc881d3275016864e
smadden5 <smadden5@gmail.com>
2022-01-26 15:16:28 -0600

    22-01-26_scott_update

--- code/group_3_jackalope.py
+++ code/group_3_jackalope.py
@@ -14,23 +14,29 @@
 population_list = [0,0]
 year = 0
 
-while year < 11:
+while year < 22:
     for i in range(len(population_list)):
         if population_list[i] >= 3:
             population_list.append(0)
         elif population_list[i] == 9:
             pass
-        elif population_list[i] == 10:
-            population_list.remove(10)
+        # elif population_list[i] == 10:
+        #     population_list.remove(10)
         population_list[i] += 1
+        for ele in population_list:  
+                if ele == 10:
+                    population_list.remove(ele)
+        
+
         # if age in population_list > 4 and age in population_list < 9:
         #     population_list.append(0)
         # if age > 3 and age < 9, new age 0 jackalopes is increased by 2 * the number of pairs in this age bracket
         # if age > 8, then no reproductive activity
         # if age > 9  population -= the pairs
-    print(population_list)
-    print(len(population_list))
+
+    print('\npopulation:',len(population_list))
     year += 1 
+    print('\nYear:', year)
 
 
 # Version 2
