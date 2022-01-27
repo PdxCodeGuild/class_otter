ed0c61b3379c56aad3ef384b452e17024d448999
Justin <justin.35f@gmail.com>
2022-01-27 12:34:28 -0800

    Road Trip - Working on v2

--- code/justin/Unit_01/Extra/Lab_01.py
+++ code/justin/Unit_01/Extra/Lab_01.py
@@ -15,30 +15,53 @@ city_to_accessible_cities = {
 }
 Traveling from one city to an adjacent one is called a hop. Let the user enter a city. Print out all the cities connected to that city.
 Then print out all cities that could be reached through two hops.
-'''
 
-city_to_accessible_cities = {
-  'Boston': {'New York', 'Albany', 'Portland'},
-  'New York': {'Boston', 'Albany', 'Philadelphia'},
-  'Albany': {'Boston', 'New York', 'Portland'},
-  'Portland': {'Boston', 'Albany'},
-  'Philadelphia': {'New York'}
+Version 2 (optional)
+Let the user enter a city and a number of hops. Print out all cities that could be reached through that number of hops.
+
+We've also mapped the travel time of each hop. When the user enters a city and a number of hops, print out the shortest travel times to
+each city. Paths with fewer hops are not necessarily those with the shorter travel times.
+
+city_to_accessible_cities_with_travel_time = {
+  'Boston': {'New York': 4, 'Albany': 6, 'Portland': 3},
+  'New York': {'Boston': 4, 'Albany': 5, 'Philadelphia': 9},
+  'Albany': {'Boston': 6, 'New York': 5, 'Portland': 7},
+  'Portland': {'Boston': 3, 'Albany': 7},
+  'Philadelphia': {'New York': 9},
+}
+'''
+import sys
+
+
+city_to_accessible_cities_with_travel_time = {
+  'Boston': {'New York': 4, 'Albany': 6, 'Portland': 3},
+  'New York': {'Boston': 4, 'Albany': 5, 'Philadelphia': 9},
+  'Albany': {'Boston': 6, 'New York': 5, 'Portland': 7},
+  'Portland': {'Boston': 3, 'Albany': 7},
+  'Philadelphia': {'New York': 9},
 }
 
 def get_connected_cities(city, hops=1):
-    all_connected_cities = set(city_to_accessible_cities[city])
-    connected_cities = set()
-    for i in range(1, hops):
-        for next_city in all_connected_cities:
-            connected_cities = connected_cities | set(city_to_accessible_cities[next_city])
-        all_connected_cities = all_connected_cities | connected_cities
-    
+    all_connected_cities = {}
+    connected_cities = city_to_accessible_cities_with_travel_time[city]
+
+    for _ in range(0, hops):
+        for next_city, distance in connected_cities.items():
+            if next_city in all_connected_cities:
+                all_connected_cities[next_city] += distance
+            else:
+                all_connected_cities[next_city] = distance
+        
+
+    if city in all_connected_cities:
+        all_connected_cities.pop(city)
+
     return all_connected_cities
 
 def pretty_string(cities):
     result = ''
     for city in cities:
-        result += f'{city}, '
+        result += f'{city} - {cities[city]}, '
 
     return result[:-2]
 
@@ -46,13 +69,21 @@ def main():
     is_running = True
     
     while is_running:
-        city = input('''\n\tAlbany\n\tBoston\n\tNew York\n\tPhiladelphia\n\tPortland\nEnter a city: ''').title()
-        hop_count = 2
-        if city in city_to_accessible_cities:
-            nearby_cities = get_connected_cities(city)
-            distant_cities = get_connected_cities(city, hop_count)
-            print(f'\nNearby cities: {pretty_string(nearby_cities)}')
-            print(f'Within {hop_count} hops: {pretty_string(distant_cities)}')
+        city = input('\n\tAlbany\n\tBoston\n\tNew York\n\tPhiladelphia\n\tPortland\nEnter a city: ').title()
+        if city in city_to_accessible_cities_with_travel_time:
+            hop_count = ''
+            try:
+                hop_count = input('Number of hops: ')
+                num_hops = int(hop_count)
+                
+                nearby_cities = get_connected_cities(city)
+                print(f'\nNearby cities: {pretty_string(nearby_cities)}')
+
+                # distant_cities = get_connected_cities(city, num_hops)
+                # print(f'Within {num_hops} hops: {pretty_string(distant_cities)}')
+            except Exception as e:
+                print(f'\nInvalid hop count [{hop_count}]')
+                print(sys.exc_info()[0])
         elif city == 'Q' or city == 'Quit':
             is_running = False
         else:
