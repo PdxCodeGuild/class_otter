209d6a47677ab632547e2f30a76d4f8de98094ea
Bruce Stull <bruce.stull@gmail.com>
2022-01-28 10:37:14 -0500

    pipenv setuptools basic example working.

---
+++ code/bruce/other_code/test_and_learning_files/click_learning_envs/package_example/setup.py
@@ -0,0 +1,32 @@
+# ********************************************** #
+#             Click with setuptools              #
+#   click pipenv setuptools package module cli   #
+#                  Version: 1.0                  #
+#              Author: Bruce Stull               #
+#                   2022-01-28                   #
+# ********************************************** #
+
+# Resources:
+    # The basics:
+        # https://click.palletsprojects.com/en/8.0.x/
+    # Packages can be used:
+        # https://click.palletsprojects.com/en/8.0.x/setuptools/
+        # pip install --editable .
+    # https://stackoverflow.com/questions/46330327/how-are-pipfile-and-pipfile-lock-used
+        # https://pipenv-fork.readthedocs.io/en/latest/basics.html#specifying-versions-of-a-package
+
+from setuptools import setup
+
+setup(
+    name='hello',
+    version='0.1.0',
+    py_modules=['hello'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'hello = hello:cli',
+        ],
+    },
+)
