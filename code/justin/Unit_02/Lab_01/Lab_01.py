7598ce3b003accd64fb033f79f02b3c6b6c7899a
Justin <justin.35f@gmail.com>
2022-02-01 12:20:32 -0800

    Added folder for lab

---
+++ code/justin/Unit_02/Lab_01/Lab_01.py
@@ -0,0 +1,27 @@
+# Full Stack Bootcamp - Unit 02 Lab 01
+# Justin Hammond, 02/01/2022
+
+'''
+Lab 1: Bio
+Write up a short bio webpage for someone or something. It can be about a celebrity, a fictionary character, a place, a species, etc. Check out the examples
+
+Part 1
+    A written introduction
+    A link to a Wikipedia article
+    A picture of them
+    A list of places where they've lived
+    A quote from them
+
+Part 2
+    Center the entire content of the body
+    Modify the padding and margins on your paragraphs to look better
+    Use a custom font
+    Add a background image
+
+Part 3
+    Pick a color scheme and modify the background, body text, and link text color.
+    Change the typeface of the quote.
+    Add a rounded border to the picture.
+    Change the bullet points on the list of places.
+'''
+
