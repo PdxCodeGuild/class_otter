1da428cd6cc71d341610e0ea3c482f82efeb8a9a
Nick Corner <nickcorner421@gmail.com>
2022-02-01 11:28:24 -0600

    lab 16

--- code/nick/python/lab16miniCapstone/lab16.py
+++ code/nick/python/lab16miniCapstone/lab16.py
@@ -1,10 +1,8 @@
-from openpyxl.styles import fonts
-from openpyxl.chart import Reference, BarChart
-from openpyxl import load_workbook
+from pathlib import Path
+import xlwings as xw
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import glob
 
 
 
@@ -12,54 +10,65 @@ import glob
 years = range(1991,2022)
 
 for year in years:
-    url = f'https://www.basketball-reference.com/leagues/NBA_{year}_per_game.html'
+       url = f'https://www.basketball-reference.com/leagues/NBA_{year}_per_game.html'
 
-    r = requests.get(url)
+       r = requests.get(url)
 
-    soup = BeautifulSoup(r.text, 'html.parser')
+       soup = BeautifulSoup(r.content, 'lxml')
 
-    table_header = [th.getText() for th in soup.find_all('tr')[0].find_all('th')]
+       header = [th.getText() for th in soup.find_all('tr')[0].find_all('th')]
+       header = header[1:]
+       rows = soup.find_all('tr')[1:]
 
-    header = table_header[1:]
+       player_stats = [[td.getText() for td in rows[i].find_all('td')] for i in range(len(rows))]
 
-    rows = soup.find_all('tr')[1:]
-    player_stats = [[td.getText()for td in rows[i].find_all('td')] for i in range(len(rows))]
+       stats = pd.DataFrame(player_stats, columns=header)
 
-    stats = pd.DataFrame(player_stats, columns=header)
-    stats.dtypes
-    stats[['MP','FG','FGA','FG%','3P','3PA','3P%','2P','2PA','2P%','eFG%','FT','FTA','FT%','ORB','DRB','TRB','AST','STL','BLK','TOV','PF','PTS']]=stats[['MP','FG','FGA','FG%','3P','3PA','3P%','2P','2PA','2P%','eFG%','FT','FTA','FT%','ORB','DRB','TRB','AST','STL','BLK','TOV','PF','PTS']].apply(pd.to_numeric)
-        # print(stats.head(10))
-        # stats['MP'] = pd.to_numeric(stats['MP'], downcast='float')
-        # print(stats.sort_values('MP', ascending=False).head(5))
-    stats.sort_values('PTS', ascending=False).head(25).to_excel(f"C:\\Users\\nickc\\Documents\\nba_stats\\nba_stats_{year}.xlsx", index=False)
+       stats[['MP','FG','FGA','FG%','3P','3PA','3P%','2P','2PA','2P%','eFG%','FT','FTA','FT%','ORB','DRB','TRB','AST','STL','BLK','TOV','PF','PTS']] = stats[['MP','FG','FGA','FG%','3P','3PA','3P%','2P','2PA','2P%','eFG%','FT','FTA','FT%','ORB','DRB','TRB','AST','STL','BLK','TOV','PF','PTS']].apply(pd.to_numeric)
+       stats = stats.sort_values('PTS', ascending=False).head(10)
+       stats.to_excel(f'C:\\Users\\nickc\\Documents\\nba_stats\\{year}_nba_stats.xlsx')
 
 
-############################################################################
+source_dir = 'C:\\Users\\nickc\\Documents\\nba_stats'
 
+excel_files = list(Path(source_dir).glob('*.xlsx'))
+combined_wb = xw.Book()
 
-# source_data = 'C:\\Users\\nickc\\Documents\\nba_stats'
+for excel_file in excel_files:
+       wb = xw.Book(excel_file)
+       for sheet in wb.sheets:
+              sheet.name=f'nba_{year}'
+              sheet.api.Copy(After=combined_wb.sheets[0].api)
+       wb.close()
 
-# stat_files = list(Path(source_data).glob('*.xlsx'))
+combined_wb.sheets[0].delete()
+combined_wb.save(f'C:\\Users\\nickc\\Documents\\nba_stats\\all_nba_stats.xlsx')
 
-# combined_wb = xw.Book()
-# for stat_file in stat_files:
-#     wb = xw.Book(stat_file)
-#     for sheet in wb.sheets:
-#         sheet.api.Copy(After=combined_wb.sheets[0].api)
-#     wb.close()
+# workbook = writer.book
+# worksheet = writer.sheets['sheet 1']
+# chart = workbook.add_chart({'type':'bar'})
 
-# combined_wb.sheets[0].delete()
-# combined_wb.save('C:\\Users\\nickc\\Documents\\nba_stats\\all_nba_stats.xlsx')
+# data = [stats['PTS']]
 
-file_path = 'C:\\Users\\nickc\\Documents\\nba_stats\\*.xlsx'
+# writer = pd.ExcelWriter('C:\\Users\\nickc\\Documents\\nba_stats\\2022_nba_stats.xlsx', engine='xlsxwriter')
 
-files = glob.glob(file_path)
+# pd.DataFrame.to_excel(stats, "C:\\Users\\nickc\\Documents\\nba_stats\\2022_nba_stats.xlsx")
 
-df = pd.DataFrame()
-for file in files:
-    tempdf = pd.read_excel(file)
-    df= df.append(tempdf, ignore_index=True)
+# with open('C:\\Users\\nickc\\Documents\\nba_stats\\2022_nba_stats.csv') as csv_file:
+#          csv_reader = csv.DictReader(csv_file)
+#          players = []
+#          points = []
+#          for row in csv_reader:
+#                 players.append(row['Player'])
+#                 points.append(row['PTS'])
 
-print(df)
+# plt.barh(players, points)
 
-pivot = pd.pivot_table
+# plt.title('Players by most points')
+# plt.ylabel('players')
+# plt.xlabel('points')
+# plt.tight_layout()
+# plt.savefig('C:\\Users\\nickc\\Documents\\nba_stats\\top_10_ppg.png')
+
+# read_file = pd.read_csv('C:\\Users\\nickc\\Documents\\nba_stats\\2022_nba_stats.csv')
+# read_file.to_excel('C:\\Users\\nickc\\Documents\\nba_stats\\2022_nba_stats.xlsx', index= None, header=True)
