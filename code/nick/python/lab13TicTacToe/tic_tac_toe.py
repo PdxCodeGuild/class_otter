fd4cbc95d0445cb0a4c6fcd772abf7ea922acae3
Nick Corner <nickcorner421@gmail.com>
2022-02-04 18:35:22 -0600

    lab 13 update

--- code/nick/python/lab13TicTacToe/tic_tac_toe.py
+++ code/nick/python/lab13TicTacToe/tic_tac_toe.py
@@ -1,25 +1,60 @@
+
 class Player:
-    def __init__(self, player_name, token):
-        self.player_name = player_name
+    def __init__(self, name, token):
+        self.name = name
         self.token = token
 
 class Game:
     def __init__(self):
-        self.game_board = [
-                ['_','_','_'],
-                ['_','_','_'],
-                ['_','_','_'],
-                ]
-    def __repr__(self):
-        for row in self.game_board:
-            for spot in row:
-                    print(spot, end='')
-                    
+        self.board = [
+            [' ',' ',' '],
+            [' ',' ',' '],
+            [' ',' ',' ']
+        ]
+
+    def __repr__(self):  
+        b = ''
+        for row in self.board:
+            b += '|'.join(row)
+            b += '\n'
+        return b
         
-    def move(self, x, y, player):
-        self.game_board[x][y] = player
+    def move(self, x, y, token):
+        if self.board[y][x] != ' ':
+            return 'invalid move'
+        else:
+            self.board[x][y] = token
+    
+    def calc_winner(self):
+        if self.board[0][0]==self.board[0][1]==self.board==[0][2]:
+            return self.board[0][0]
+
+
+    
+    # def is_full():
+    
+    # def is_game_over():
   
 
 
-board = Game()
-print(board.move(2,2,'x'),board.__repr__())
+# player_1 = Player(input('player 1 name: '), 'X')
+# player_2 = Player(input('Player 2 name: '), 'O')
+# print(player_1.name,player_1.token)
+# print(player_2.name,player_2.token)
+# print(repr(game.move(0,0, player_1.token)))
+# print(repr(game))
+
+while True:
+    board = Game()
+    first = Player(input('player one: '),'X')
+    second = Player(input('player two: '),'O')
+    print(first.name, first.token)
+    print(second.name, second.token)
+    print('Coordinates are 0, 1, and 2.\n0,0 being the top left of the board. ')
+    while True:
+        x = input('enter coordinates x: ').strip()
+        y = input('enter coordinate y: ').strip()
+        x=int(x)
+        y=int(y)
+        board.move(x,y,first.token)
+        print(repr(board))
