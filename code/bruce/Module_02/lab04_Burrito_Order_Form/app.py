2d3b11dec2c3634fc9c28b8431d363ac1562917b
Bruce Stull <bruce.stull@gmail.com>
2022-02-08 18:54:30 -0500

    Lab 4: Burrito Order Form - finished functionality. Need to style the page.

--- code/bruce/Module_02/lab04_Burrito_Order_Form/app.py
+++ code/bruce/Module_02/lab04_Burrito_Order_Form/app.py
@@ -6,9 +6,22 @@
 #                  2022-02-08                  #
 # ******************************************** #
 
+from flask import Flask, render_template
+
 # Assignment:
 # https://github.com/PdxCodeGuild/class_otter/blob/bruce/2%20Flask%20%2B%20HTML%20%2B%20CSS/labs/04%20Burrito%20Order%20Form.md
 
-# Start pipenv:
-# source /c/Users/Bruce/.virtualenvs/flask_learning-E-mT5RQG/Scripts/activate
+# Activate virtual environment (in BASH):
+# source C:/Users/Bruce/.virtualenvs/lab04_Burrito_Order_Form-b4iS58LK/Scripts/activate
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+
+app.run(debug=True)
