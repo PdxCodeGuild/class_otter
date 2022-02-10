406521d26decb8d861125e10011243d1ffa032df
Justin <justin.35f@gmail.com>
2022-02-10 10:57:49 -0800

    Lab 05 - Updated logic to include cipher rotation_cipher

--- code/justin/Unit_02/Lab_05/app.py
+++ code/justin/Unit_02/Lab_05/app.py
@@ -3,19 +3,20 @@ from rotation_cipher import RotationCipher
 
 
 app = Flask(__name__)
-  
+max_rotation = RotationCipher.allowed_characters_size
+
 @app.route('/')
 def index():
     if len(request.args) > 0:
         encoded_message = request.args['encoded_message']
         if encoded_message is not None:
-            return render_template('index.html', should_hide='', encoded_message=encoded_message)
+            return render_template('index.html', should_hide='', encoded_message=encoded_message, max_rotation=max_rotation)
     else:
-        return render_template('index.html', should_hide='hide', encoded_message='')
+        return render_template('index.html', should_hide='hide', encoded_message='', max_rotation=max_rotation)
 
 @app.route('/encode/', methods=['POST'])
 def encode():
-    encoded_message = RotationCipher.encrypt(request.form['message'])
+    encoded_message = RotationCipher.encrypt(request.form['message'], int(request.form['rotation_value']))
     return redirect(f'/?encoded_message={encoded_message}')
 
 
