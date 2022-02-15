fa8efd2a662c1d27d676285b8a81d487ba298108
Nick Corner <nickcorner421@gmail.com>
2022-02-15 11:47:04 -0600

    update labo5

--- code/nick/html-css-flask/flask labs/lab05FlaskRedo/lab05Redo.py
+++ code/nick/html-css-flask/flask labs/lab05FlaskRedo/lab05Redo.py
@@ -1,18 +1,16 @@
-import re
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
-metrics ={'ft': 0.3048, 'in': 1609.34, 'mi': 1609.34, 'm': 1, 'km': 1000}
+metrics ={'ft': 0.3048, 'in': .9144, 'yd': .0254, 'mi': 1609.34, 'm': 1, 'km': 1000}
 
-output = []
+output = {}
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         user_distance = float(request.form['number'])
         unit = request.form['unit']
         conversion= user_distance * metrics[unit]
-        output.append(conversion)
-
+        output[unit] = conversion
         return redirect('/')
     return render_template('index.html',output=output,metrics=metrics)
 
