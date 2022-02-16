7f576488ccb3f178d24b4e12a67d36a49814eaec
Justin <justin.35f@gmail.com>
2022-02-16 13:04:16 -0800

    Results page bug fix

--- code/justin/Unit_03/Lab_01/polls/views.py
+++ code/justin/Unit_03/Lab_01/polls/views.py
@@ -29,4 +29,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
