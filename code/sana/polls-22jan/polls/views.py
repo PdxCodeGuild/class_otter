1bff4d4433deb400b6223750505529d66ee9e2b3
Sana Rahmani <sanarahmani@live.com>
2022-02-17 12:54:17 -0600

    company lab

--- code/sana/polls-22jan/polls/views.py
+++ code/sana/polls-22jan/polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from .models import Question
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -8,7 +8,8 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You are looking question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question' : question})
 
 def results(request, question_id):
     return HttpResponse(f"You are looking at results of question {question_id}")
