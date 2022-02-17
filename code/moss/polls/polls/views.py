4f1b9aa6d359d458eacfa7c3d22668a7a2a46657
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-02-16 23:14:25 -0800

    Django setup

--- code/moss/polls/polls/views.py
+++ code/moss/polls/polls/views.py
@@ -3,7 +3,22 @@
 # # Create your views here.
 
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Question
 
 def index(request):
-    return HttpResponse('Hello World!!!')
+    # return HttpResponse('Hello World!!!')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'lates_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
+
+def detail(request, question_id):
+    return HttpResponse(f"Your're looking at question {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of the question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Your're voting on question {question_id}")
