b5efa43f644ea78219872b4719b81d7d93e8c1af
Sana Rahmani <sanarahmani@live.com>
2022-02-16 18:27:22 -0600

    commit

--- code/sana/polls-22jan/polls/admin.py
+++ code/sana/polls-22jan/polls/admin.py
@@ -1,3 +1,6 @@
+from tkinter.messagebox import QUESTION
 from django.contrib import admin
-
+from .models import Question, Choice
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Choice)
--- code/sana/polls-22jan/polls/views.py
+++ code/sana/polls-22jan/polls/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('hello world')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {"latest_question_list" : latest_question_list}
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    return HttpResponse(f"You are looking question {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"You are looking at results of question {question_id}")
+
+def voting(request, question_id):
+    return HttpResponse(f"You are looking at the votes on question {question_id}")
