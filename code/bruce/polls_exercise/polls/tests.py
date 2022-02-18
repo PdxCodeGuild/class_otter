fbaec86e274ec3f7015dfe32b2c5249a97b6c2f4
Bruce Stull <bruce.stull@gmail.com>
2022-02-18 18:29:05 -0500

    Django Example working with tests and colored background. Will attempt to use generic views working again and have them work properly with the tests.

--- code/bruce/polls_exercise/polls/tests.py
+++ code/bruce/polls_exercise/polls/tests.py
@@ -1,10 +1,17 @@
 import datetime
+from urllib import response
 import django
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Question
+
+def create_question(question_text, days):
+    # 'timezone' and 'datetime' seem to have same format.
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 class QuestionModelTests(TestCase):
 
@@ -33,4 +40,60 @@ class QuestionModelTests(TestCase):
         recent_question = Question(pub_date=time_within_twenty_four_hours_ago)
         self.assertIs(recent_question.was_published_recently(), True)
 
+
+class IndexViewTests(TestCase):
+    
+    def test_no_questions(self):
+        response = self.client.get(reverse('polls:index'))
+        print(reverse('polls:index'))  # /polls/
+        # print(reverse('polls:detail', {'pk': 1}))  #
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+    
+    def test_past_question(self):
+        question = create_question(question_text="Past question.", days=-10)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, question.question_text)
+        self.assertQuerysetEqual(response.context['latest_question_list'], [question])
+    
+    def test_future_question(self):
+        create_question(question_text="Future question.", days=10)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+    
+    def test_future_question_and_past_question(self):
+        past_question = create_question(question_text="Past question.", days=-10)
+        future_question = create_question(question_text="Future question", days=10)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, past_question.question_text)
+        self.assertNotContains(response, future_question.question_text)
+        self.assertQuerysetEqual(response.context['latest_question_list'], [past_question])
+    
+    def test_two_past_questions(self):
+        first_past_question = create_question(question_text="First past question.", days=-10)
+        second_past_question = create_question(question_text="Second past question.", days=-31)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, first_past_question.question_text)
+        self.assertContains(response, second_past_question.question_text)
+        # Order is important since we have two items in query set.
+        self.assertQuerysetEqual(response.context['latest_question_list'], [first_past_question, second_past_question])
+
+
+class DetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text="Future question.", days=1)
+        # Using list for 'args='
+        response = self.client.get(reverse('polls:detail', args=[future_question.id]))
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text="Past question", days=-1)
+        # Using tuple for 'args='
+        response = self.client.get(reverse('polls:detail', args=(past_question.id,)))
+        self.assertContains(response, past_question.question_text)
+        self.assertEqual(response.context['question'], past_question)
+
+    
 
--- code/bruce/polls_exercise/polls/views.py
+++ code/bruce/polls_exercise/polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from .models import Question, Choice
+from django.utils import timezone
 
 # Use generic views:
 # https://docs.djangoproject.com/en/4.0/intro/tutorial04/
@@ -15,18 +16,37 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         # "get_queryset()" is the required function name for Django to know what to do with data result
         # 'latest_question_list' can be any variable, it doesn't seem to be a required naming convention for Django. We use it above since that's what we already have on the template html.
-        latest_question_list = Question.objects.order_by('-pub_date')[:5]
+        latest_question_list = Question.objects.filter(
+            pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
         return latest_question_list
 
 
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
+def detail(request, question_id):
+    # Old way of getting 'Question':
+        # question = Question.objects.get(pk=question_id)
+    # A better way, from tutorial:
+    # Adds error handling of 404, returns the 'Question', otherwise.
+    question = get_object_or_404(
+        Question.objects.filter(pub_date__lte=timezone.now()),
+        pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
 
+# class DetailView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/detail.html'
 
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
+def results(request, question_id):
+    question = get_object_or_404(
+        Question.objects.filter(pub_date__lte=timezone.now()),
+        pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/results.html', context)
+
+# class ResultsView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
