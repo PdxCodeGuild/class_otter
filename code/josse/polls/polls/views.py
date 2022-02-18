16df38fe26fbb673567c6fa1b4698b690bbb2665
Josse <perez.josse@hotmail.com>
2022-02-18 18:25:24 -0500

    finished django example

--- code/josse/polls/polls/views.py
+++ code/josse/polls/polls/views.py
@@ -1,20 +1,30 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
 
 from .models import Question, Choice
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, 'polls/index.html',context)
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(
+        Question.objects.filter(pub_date__lte=timezone.now()),
+     pk=question_id
+     
+     )
     return render(request, 'polls/detail.html',{"question" : question})
 
 def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(
+       Question.objects.filter(pub_date__lte=timezone.now()),
+     pk=question_id
+     
+       
+        )
     return render(request, 'polls/results.html',{"question" : question})
 
 def vote(request, question_id):
@@ -27,3 +37,7 @@ def vote(request, question_id):
     selected_choice.votes += 1
     selected_choice.save()
     return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+# def my_page(request):
+#     pass_variable = "hello world"
+#     return render(request, 'polls/my_page.html',{"pass_variable" : pass_variable })
