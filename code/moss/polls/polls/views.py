ebf2a63862e659ed84d5e1c42f926d13e66f67b0
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-02-17 23:25:10 -0800

    Django setup

--- code/moss/polls/polls/views.py
+++ code/moss/polls/polls/views.py
@@ -1,24 +1,31 @@
-# from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-# # Create your views here.
-
-from django.http import HttpResponse
-from django.shortcuts import render
-
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
-    # return HttpResponse('Hello World!!!')
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'lates_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
-
 def detail(request, question_id):
-    return HttpResponse(f"Your're looking at question {question_id}")
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of the question {question_id}")
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+    
 def vote(request, question_id):
-    return HttpResponse(f"Your're voting on question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choices.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            'question': question,
+            'error_message': "Please select a valid choice."
+        })
+    selected_choice.votes +=1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
