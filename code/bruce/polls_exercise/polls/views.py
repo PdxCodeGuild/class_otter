63cba7fe6c3af3b726867c2a99b7beb50041c042
Bruce Stull <bruce.stull@gmail.com>
2022-02-17 21:14:41 -0500

    Django Example - Implemented generic veiws. Seems to be working properly.

--- code/bruce/polls_exercise/polls/views.py
+++ code/bruce/polls_exercise/polls/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 
 from django.shortcuts import get_object_or_404, render
 
 from django.urls import reverse
+
+from django.views import generic
 
 from .models import Question, Choice
 
@@ -10,36 +12,44 @@ from .models import Question, Choice
 # https://docs.djangoproject.com/en/4.0/intro/tutorial04/
 
 
-def index(request):
-    # '-pub_date' will order by pub_date descending:
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    print(latest_question_list)
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+
+class IndexView(generic.ListView):
+    # Assign some specific variables instead of passing values into 'render()'.
+    # Specify the template_name:
+    template_name = 'polls/index.html'
+    # This is the variable name used in the template html, specified above.
+    # 'latest_question_list' used here is important, it has to match the variable in the template html.
+    # Specify the context_object_name:
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        """Return the last five published questions."""
+        # '-pub_date' will order by pub_date descending:
+        latest_question_list = Question.objects.order_by('-pub_date')[:5]
+        # 'latest_question_list' above and below can be anything, it's only used inside this class.
+        return latest_question_list
 
 
-def detail(request, question_id):
+# Old detail() view:
     # Old way of getting 'Question':
         # question = Question.objects.get(pk=question_id)
     # A better way, from tutorial:
     # Adds error handling of 404, returns the 'Question', otherwise.
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return render(request, 'polls/detail.html', context)
+    # question = get_object_or_404(Question, pk=question_id)
+    # context = {'question': question}
+    # return render(request, 'polls/detail.html', context)
+
+# def detail(request, question_id):
+class DetailView(generic.DetailView):
+    # We specify 'model' as 'Question', so we don't need to tell Django a 'context' dictionary like we did before. 'Question' class has all the information needed to get stuff from any given 'question'. I'm guessing the 'context_object_name' is automagically 'question' (lower-case of class 'Question').
+    model = Question
+    # Need to assign the template html to 'template_name'.
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return render(request, 'polls/results.html', context)
-
-# def results(request, question_id):
-#     '''Display the 'choice's and votes for a question?'''
-#     question = Question.objects.get(pk = question_id)
-#     choices_list = question.choices.all()
-#     context = {'choices_list': choices_list}
-#     print(choices_list)
-#     return render(request, 'polls/results.html', context)
+# Similar to detail() view, just need the proper 'template_name':
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
