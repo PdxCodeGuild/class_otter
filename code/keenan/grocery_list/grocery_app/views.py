c329d5d91cf8246665ef9ac881e2bfc23add86e9
ktabusa <ktabusa@gmail.com>
2022-02-22 16:51:00 -0800

    completed project set up

--- code/keenan/grocery_list/grocery_app/views.py
+++ code/keenan/grocery_list/grocery_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import GroceryItem
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('ok')
+    item_list = GroceryItem.objects.all()
+    context = {'item_list': item_list}
+    return render(request, 'grocery_app/list.html', context)
+
+
