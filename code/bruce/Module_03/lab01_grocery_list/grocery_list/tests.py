c93944fd8e377a987ac0d0eb3dd39dee7b32d5eb
Bruce Stull <bruce.stull@gmail.com>
2022-02-24 15:19:34 -0500

    Fixed bug in Lab 01 : Grocery List.

--- code/bruce/Module_03/lab01_grocery_list/grocery_list/tests.py
+++ code/bruce/Module_03/lab01_grocery_list/grocery_list/tests.py
@@ -104,7 +104,7 @@ class IndexViewTests(TestCase):
 
     def test_no_grocery_items(self):
         """
-        IndexView() returns 'No grocery items available.' with a 200 status
+        IndexView() returns 'No uncompleted grocery items available.' with a 200 status
         when no grocery items are available.
         """
         # GET the response for 'grocery_list' 'index':
@@ -112,8 +112,8 @@ class IndexViewTests(TestCase):
         # Response should have status code of 200:
         # print(f"response.status_code: {response.status_code}")
         self.assertEqual(response.status_code, 200)
-        # Response should contain text 'No grocery items available.':
-        self.assertContains(response, 'No grocery items available.')
+        # Response should contain text 'No uncompleted grocery items available.':
+        self.assertContains(response, 'No uncompleted grocery items available.')
 
     def test_one_grocery_item_present(self):
         """
@@ -142,62 +142,102 @@ class IndexViewTests(TestCase):
         # Response should contain the text 'Another New Grocery Item':
         self.assertContains(response, 'Another New Grocery Item')
 
-    # TODO: Actually test the 'complete' view and not the model method.
-    # def test_complete(self):
-    #     item = new_grocery_item("A grocery item")
-    #     # NOTE: Big Dummy!!! Forgot to 'item.save()'
-    #     item.complete_item()
-    #     # LOL Remember to save.
-    #     item.save()
-    #     response = self.client.get(reverse('grocery_list:index'))
-    #     # Getting info from database.
-    #     self.assertEquals(item.completed, True)
-    #     # What value is being sent to template.
-    #     self.assertEquals(response.context['completed_groceries'][0].completed, True )
-
     def test_add_view(self):
         """
         add() adds item to list and redirects user to 'index' where item
         is displayed.
         """
         # Create dictionary to submit with POST request:
-        grocery_item_to_add = {'description': "A grocery item"}
+        grocery_description_to_add = {'description': "A grocery item"}
         # Submit the POST request:
-        self.client.post(reverse('grocery_list:add'), grocery_item_to_add)
+        self.client.post(reverse('grocery_list:add'), grocery_description_to_add)
         # GET the response:
         response = self.client.get(reverse('grocery_list:index'))
         # Verify item 'A grocery item' shows up in 'response':
         self.assertContains(response, "A grocery item")
 
+    def test_complete_view(self):
+        # Add a new item to grocery list:
+        # item = new_grocery_item("A grocery item")
+        new_grocery_item("A grocery item")
+        # GET the response:
+        response = self.client.get(reverse('grocery_list:index'))
+        # Verify item 'A grocery item' shows up in 'response':
+        self.assertContains(response, "A grocery item")
+        # key_to_complete = response.context['uncompleted_groceries'][0].pk
+        # Get the item we want to complete:
+        item_to_complete = response.context['uncompleted_groceries'][0]
+        # self.client.post(reverse('grocery_list:complete', args=[key_to_complete]))
+        # Submit request to 'complete' the item, by passing the pk:
+        self.client.post(reverse('grocery_list:complete', args=[item_to_complete.pk]))
+        # Get the response:
+        response = self.client.get(reverse('grocery_list:index'))
+        # Getting info from database.
+        # self.assertEquals(GroceryItem.objects.get(pk=item_to_complete.id).completed, True)
+        self.assertTrue(GroceryItem.objects.get(pk=item_to_complete.id).completed)
+        # NOTE: Maybe get 'completed_groceries' from context?
+        # self.assertEquals(response.context['completed_groceries'][0].completed, True )
+        self.assertTrue(response.context['completed_groceries'][0].completed)
+
+    # TODO: Are we actually testing the view?
     def test_delete_view(self):
         """
         delete() removes item from list view and returns user to 'index'
         view where the item is no longer displayed.
         """
+        # We use 'description' here since that is the 'key' the 'add()' view is using to get the value.
         # Create dictionary to submit with POST request:
-        grocery_item_to_add = {'description': "A grocery item"}
-        # Submit the POST request:
-        self.client.post(reverse('grocery_list:add'), grocery_item_to_add)
+        grocery_description_to_add = {'description': "A grocery item"}
+
+        #### Uses 'add()' to add the item ####
+        # Submit the POST request to 'add()' view:
+        self.client.post(reverse('grocery_list:add'), grocery_description_to_add)
+
+        # #### Notes ####
+        # The POST request doesn't return much.
+        # request_maybe = self.client.post(reverse('grocery_list:add'), grocery_description_to_add)
+        # # print(f"request_maybe: {request_maybe}")
+        # # # request_maybe: <HttpResponseRedirect status_code=302, "text/html; charset=utf-8", url="/grocery-list/">
+        # # print(f"request_maybe.context: {request_maybe.context}")
+        # # # request_maybe.context: None
+
         # GET the response:
         response = self.client.get(reverse('grocery_list:index'))
         # Verify item 'A grocery item' shows up in 'response':
         self.assertContains(response, "A grocery item")
 
-        print(f"response.context['deletable_groceries']: {response.context['deletable_groceries']}")
-        # response.context['deletable_groceries']: <QuerySet [<GroceryItem: 1: A grocery item - Completed[False]>]>
-        print(f"response.context['deletable_groceries'][0]: {response.context['deletable_groceries'][0]}")
-        # response.context['deletable_groceries'][0]: 1: A grocery item - Completed[False]
-        print(f"response.context['deletable_groceries'][0].pk: {response.context['deletable_groceries'][0].pk}")
-        # response.context['deletable_groceries'][0].pk: 1
+        # #### Notes ####
+        # # Can use 'response.context['deletable_groceries']' to get 'deletable_groceries' which is a 'QuerySet'. That 'QuerySet' contains a list which has one item in it. That item is a 'GroceryItem'. From a user standpoint, there will usually be more than one 'GroceryItem' so the size of 'QuerySet' will be typically larger than one. It is one here since we are testing and we only added one 'GroceryItem'.
+        # print(f"response.context['deletable_groceries']: {response.context['deletable_groceries']}")
+        # # response.context['deletable_groceries']: <QuerySet [<GroceryItem: 1: A grocery item - Completed[False]>]>
+        # print(f"type(response.context['deletable_groceries']): {type(response.context['deletable_groceries'])}")
+        # # type(response.context['deletable_groceries']): <class 'django.db.models.query.QuerySet'>
 
-        key_to_delete = response.context['deletable_groceries'][0].pk
+        # # Can use 'response.context['deletable_groceries'][0]' to get the single 'GroceryItem' within 'deletable_groceries' a 'QuerySet'. We can hard-code '0' for index/key since, in test, we are adding exactly one item.
+        # print(f"response.context['deletable_groceries'][0]: {response.context['deletable_groceries'][0]}")
+        # # response.context['deletable_groceries'][0]: 1: A grocery item - Completed[False]
+        # print(f"type(response.context['deletable_groceries'][0]): {type(response.context['deletable_groceries'][0])}")
+        # # type(response.context['deletable_groceries'][0]): <class 'grocery_list.models.GroceryItem'>
+        # print(f"response.context['deletable_groceries'][0].pk: {response.context['deletable_groceries'][0].pk}")
+        # # response.context['deletable_groceries'][0].pk: 1
 
+        # Get the key for the item we just added to we know which item to delete.
+        item_to_delete = response.context['deletable_groceries'][0]
+        # key_to_delete = response.context['deletable_groceries'][0].pk
+
+        # print(f"type(key_to_delete): {type(key_to_delete)}")
+        # # type(key_to_delete): <class 'int'>
+
+        # We can use 'args=[key_to_delete]' here since '<app>urls.py' expects an integer, which 'type(key_to_delete)' is. We don't need to specify any keys or indices. '<app>urls.py' assigns the value, we passed in args, to the 'key', 'pk', and passes it to 'delete()' view.
+        self.assertFalse(GroceryItem.objects.get(pk=item_to_delete.pk).deleted)
+        self.assertNotContains(response, 'No deletable grocery items available.')
         # Delete the grocery item:
-        grocery_item_to_delete = {'1': 1}
-        # self.client.post(reverse('grocery_list:delete'), grocery_item_to_delete)
-        self.client.post(reverse('grocery_list:delete', args=[key_to_delete]))
+        self.client.post(reverse('grocery_list:delete', args=[item_to_delete.pk]))
+        # self.client.post(reverse('grocery_list:delete', args=[key_to_delete]))
+        # Verify item had 'deleted' attribute set to 'True' in database:
+        self.assertTrue(GroceryItem.objects.get(pk=item_to_delete.pk).deleted)
         # GET the response:
         response = self.client.get(reverse('grocery_list:index'))
         # Response should contain text 'No grocery items available.':
-        self.assertContains(response, 'No grocery items available.')
+        self.assertContains(response, 'No deletable grocery items available.')
 
