d59d70a3dc699b1f6a05adffee40e191dcc27bba
Jacob <anduril18@hotmail.com>
2022-02-23 16:08:23 -0800

    Django Lab 1

--- code/jacob/DJango/GroceryList/grocery_list/glist/urls.py
+++ code/jacob/DJango/GroceryList/grocery_list/glist/urls.py
@@ -6,4 +6,8 @@ app_name = 'glist'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create/', views.create_item, name='create'),
+    path('<int:grocery_item_id>/delete/', views.delete_item, name='delete'),
+    path('<int:grocery_item_id>/complete/', views.mark_complete, name='complete')
+
 ]
--- code/jacob/DJango/GroceryList/grocery_list/glist/views.py
+++ code/jacob/DJango/GroceryList/grocery_list/glist/views.py
@@ -1,20 +1,37 @@
-from django.shortcuts import render, get_list_or_404, reverse
+from django.shortcuts import get_object_or_404, render, get_list_or_404, reverse
 
 # Create your views here.
 
 from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import GroceryItem
+from django.urls import reverse
+from django.utils import timezone
 
 def index(request):
-    return HttpResponse('ok')
+    latest_grocery_list = GroceryItem.objects.filter(post_date__lte=timezone.now())
+    completed_items = latest_grocery_list.filter(is_completed=True)
+    not_completed = latest_grocery_list.filter(is_completed=False)
+    context = {'latest_grocery_list': latest_grocery_list, 'not_completed': not_completed, 'completed_items': completed_items}
+    return render(request, 'glist/index.html', context)
 
-def additem(request, grocery_item_id):
-    grocery_list = GroceryItem.objects.get(pk=grocery_item_id)
+def create_item(request):
+    description = request.POST['description']
+    GroceryItem.objects.create(grocery_item=description, post_date=timezone.now())
+    return HttpResponseRedirect(reverse('glist:index'))
+
+def mark_complete(request, grocery_item_id):
+    grocery_item = get_object_or_404(GroceryItem, pk=grocery_item_id)
+    if grocery_item.is_completed == True:
+        grocery_item.is_completed = False
     
+    else:
+        grocery_item.is_completed = True
 
-def create
+    grocery_item.save()
+    return HttpResponseRedirect(reverse('glist:index'))
 
-def completed
-
-def delete
+def delete_item(request, grocery_item_id):
+    grocery_item = get_object_or_404(GroceryItem, pk=grocery_item_id)
+    grocery_item.delete()
+    return HttpResponseRedirect(reverse('glist:index'))
