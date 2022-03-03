ccfac2baa20375b2069da98504e2f1f182e35f1e
Bruce Stull <bruce.stull@gmail.com>
2022-03-03 05:37:43 -0500

    Added Favicon to URL Shortener and Chirp.

--- code/bruce/Module_03/lab03_chirp/chirps/models.py
+++ code/bruce/Module_03/lab03_chirp/chirps/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.urls import reverse
 
 class Chirp(models.Model):
-    # title = models.CharField(max_length=200) # There isn't really a title for Chirps.
     author = models.ForeignKey('auth.User', related_name='chirps', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
-    # updated = models.DateTimeField(auto_now=True)
+    # Using 'updated' here so we can track when Chirps are 'moderated'. Even though the 'Chirp's aren't typically 'updated'.
+    updated = models.DateTimeField(auto_now=True)
     tiny_body = models.TextField('Tiny Body', max_length=127)
 
     def __str__(self):
