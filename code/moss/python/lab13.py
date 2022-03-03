519904bed85f9ae7287946d2604d83b1aa4f8950
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-03-02 23:18:43 -0800

    Lab13

--- code/moss/python/lab13.py
+++ code/moss/python/lab13.py
@@ -1,18 +1,37 @@
 class player:
-    name = #player name
-    token = ["X",'O']
 
-class game:
-    board = #representation of board
-    __repr__()
-
-    move(x,y,player)
-
-    calc_winner()
-
-    is_full()
+    def __init__(self, name, token):
+        
+        self.name = name
+        self.token = token
     
-    is_game_over()
+    def __repr__(self):
+        return self.name
+
+class Game:
+    
+    def __init__(self):
+        
+        self,board = {"1": " ", "2": " ", "3": " ", "4": " ",
+        "5": " ", "6": " ", "7": " ", "8": " ", "9": " "}
+
+    def __repr__(self):
+        
+        print(self.board["1"] + "|") + self.board["2"] + "|" + self.board["3"] + "|")
+        print("-+-+-")
+        print(self.board["4"] + "|" + self.board["5"] + "|" + self.board["6"] + "|")
+        print("-+-+-")
+        print(self.board["7"] + "|" + self.board["8"] + "|" + self.board["9"] + "|")
+        print("-+-+-")
+        print(self.board["10"] + "|" + self.board["11"] + "|" + self.board["12"] + "|")
+    
+    def move(self, player):
+    
+    def calc_winner(self)
+
+    def is_full(self)
+    
+    def is_game_over(self)
 
     
 
