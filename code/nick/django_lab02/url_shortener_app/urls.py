065510158fc805af78c32b89871923896a284b25
Nick Corner <nickcorner421@gmail.com>
2022-03-02 18:34:59 -0600

    django lab02

--- code/nick/django_lab02/url_shortener_app/urls.py
+++ code/nick/django_lab02/url_shortener_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'url_shortener_app'
+
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('new/', views.new,name='new'),
+    path('', views.short_url,name='short_url'),
+    path('<str:short_url>/',views.forward,name='forward')
 ]
--- code/nick/django_lab02/url_shortener_app/views.py
+++ code/nick/django_lab02/url_shortener_app/views.py
@@ -1,20 +1,23 @@
-from types import CodeType
-from django import urls
+import random
+import string
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from .models import Url
 from django.urls import reverse
+from .models import UrlInfo
 
-def index(request):
-    context = {'message': "blah blah blah"}
-    return render(request, 'url_shortener_app/index.html', context)
-
-
-def new(request):
-    url = request.POST['description']    
-    Url.objects.create(url=url)
-    return HttpResponseRedirect(reverse('url_shortener_app:index'))
 # Create your views here.
-# input/output for url
-# convert long code to short code
-# url needs to be recorded in database
+    # renders template
+
+def short_url(request): 
+    if request.method =='POST':
+        long_url = request.POST['long_url']
+        short_url = ''.join(random.choice(string.ascii_letters)for x in range(5))
+        new_url = UrlInfo(long_url=long_url, short_url=short_url)
+        new_url.save()
+    context = {'long_url':UrlInfo.objects.all()}
+    #    0 return render(request, 'url_shortener_app/index.html',context)
+    return render(request, 'url_shortener_app/index.html',context)
+    # places it in database and generates short code
+def forward(request):
+    return HttpResponseRedirect()
