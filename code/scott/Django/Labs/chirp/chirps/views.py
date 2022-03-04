5a4524fd83d96e03b35b259151268540973c305a
smadden5 <smadden5@gmail.com>
2022-03-03 18:38:04 -0600

    22-03-03 - Django

--- code/scott/Django/Labs/chirp/chirps/views.py
+++ code/scott/Django/Labs/chirp/chirps/views.py
@@ -21,6 +21,11 @@ class ChirpCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['chirp_list'] = Chirp.objects.all()[:5]
+        return context
 
 class ChirpEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Chirp
