ac4f124fcb712a46da46932b5d31050dfd4bd5eb
Sana Rahmani <sanarahmani@live.com>
2022-03-04 20:03:13 -0600

    Lab1 Django Functional

--- code/sana/django/grocerylistlab/glproject/glapp/urls.py
+++ code/sana/django/grocerylistlab/glproject/glapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('delete/<str:pk>', views.delete, name='delete' ),
-    path('new-item/', views.newitem, name="newitem")
+    path('new-item/', views.index, name="newitem"),
+    path('completed/<str:pk>', views.completed, name='completed' ),
 ]
--- code/sana/django/grocerylistlab/glproject/glapp/views.py
+++ code/sana/django/grocerylistlab/glproject/glapp/views.py
@@ -5,14 +5,15 @@ from .models import GLapp
 
 # Create your views here.
 def index(request):
+    print("request", request.POST)
     groitem = GLapp.objects.all()
     if request.method == 'POST':
         new_item = GLapp(
             name = request.POST['name'],
             description = request.POST['description'],
-            update = request.POST['update'],
-            add_date = request.POST['add_date'],
-            completion = request.POST['completion'],
+            # update = request.POST['update'],
+            # add_date = request.POST['add_date'],
+            # completion = request.POST['completion'],
         )
         new_item.save()
         return redirect('/')
@@ -24,6 +25,8 @@ def delete(request, pk):
     groitem.delete()
     return redirect('/')
 
-def newitem(request):
-    context = {}
-    return render(request, '/glproject/templates/index.html', context)
+def completed(request, pk):
+    groitem = GLapp.objects.get(id=pk)
+    groitem.completion = not groitem.completion
+    groitem.save()
+    return redirect('/')
