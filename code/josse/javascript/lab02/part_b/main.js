8c8696dab7fce2ff6337393347b286d70600601d
Josse <perez.josse@hotmail.com>
2022-03-08 22:25:43 -0500

    part b updated

---
+++ code/josse/javascript/lab02/part_b/main.js
@@ -0,0 +1,29 @@
+let wordInput = document.getElementById('word-input');
+let runBt = document.getElementById('run-bt');
+let outputDiv = document.getElementById('output-div');
+
+let english = {
+    "a": "n", "b": "o", "c": "p", "d": "q", "e": "r", "f": "s", "g": "t", "h": "u", "i": "v", "j": "w", "k": "x", "l": "y", "m": "z",
+    "n": "a", "o": "b", "p": "c", "q": "d", "r": "e", "s": "f", "t": "g", "u": "h", "v": "i", "w": "j", "x": "k", "y": "l", "z": "m"
+}
+
+function displaytext() {
+    console.log('hello, Bruce')
+}
+// console.log(wordInput)
+// console.log(outputDiv)
+// console.log(runBt)
+let characters = []
+
+runBt.addEventListener("click", function () {
+    let textValue = wordInput.value
+    console.log(textValue)
+    // for loop grabs each character of text value
+    for (char of textValue)
+        // characters.push adds to empty list/parameter compares to dictionary
+        characters.push(english[char])
+
+
+    outputDiv.innerText = characters.join('')
+
+})
