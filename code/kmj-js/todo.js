8d4236e510abe219a96369869da36f0dae596ebf
ktabusa <ktabusa@gmail.com>
2022-03-10 12:31:01 -0800

    added button Shells

--- code/kmj-js/todo.js
+++ code/kmj-js/todo.js
@@ -2,15 +2,26 @@ let add = document.getElementById("add")
 let remove = document.getElementById("remove")
 let complete = document.getElementById("complete")
 let todo = document.getElementById("todo")
-let completed = document.getElementById("completed")
+let completed = document.getElementById("completed_list")
 let type_text = document.getElementById("type_text")
+let item_list = []
+
 
 
 add.addEventListener('click', function(){
-    todo.innerText += type_text.value
-
-    
+    node = document.createElement("li");
+    list_item = document.createTextNode(type_text.value);
+    node.appendChild(list_item);
+    let complete = document.createElement("button");
+    complete.innerHTML = "Complete";
+    node.appendChild(complete);
+    let remove = document.createElement("button");
+    remove.innerHTML = "Remove";
+    node.appendChild(remove);
+    document.getElementById("item_list").appendChild(node);
 })
+
+
 
 remove.addEventListener('click', function(){
     todo.remove()
