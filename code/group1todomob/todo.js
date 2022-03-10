9a29f92f3b7ea8de89dbb4a4e8d48494f578b0f3
Nick Corner <nickcorner421@gmail.com>
2022-03-10 16:37:11 -0600

    updated

--- code/group1todomob/todo.js
+++ code/group1todomob/todo.js
@@ -5,7 +5,7 @@ function addNewitem() {
 
     let newItem = document.getElementById("newListItem").value
     let listItem = document.createElement("li")
-    listItem.innerHTML = `<button>Complete</button>${newItem}<button onclick='removeItem(this)'>Remove</button>`
+    listItem.innerHTML = `<button onclick='completeItem(this)'>Complete</button>${newItem}<button onclick='removeItem(this)'>Remove</button>`
     listItem.setAttribute('id', `list_item${i}`)
     i++
     incomplete.appendChild(listItem)
@@ -14,4 +14,9 @@ function addNewitem() {
 function removeItem(element) {
     element.parentElement.remove()
 
+}
+function completeItem(element) {
+    completed.appendChild(element.parentElement)
+    element.disabled = 'disabled'
+    element.parentElement.classList.add('marked')
 }
