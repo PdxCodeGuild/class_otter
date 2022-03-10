33f1894668dc2c7c34f9d3f0c018fb24b8667fcf
Nick Corner <nickcorner421@gmail.com>
2022-03-10 14:50:24 -0600

    update

--- code/group1todomob/todo.js
+++ code/group1todomob/todo.js
@@ -1,13 +1,17 @@
-
-
 let completed = document.getElementById('completeList')
 let incomplete = document.getElementById('incompleteList')
+let i = 0
+function addNewitem() {
 
+    let newItem = document.getElementById("newListItem").value
+    let listItem = document.createElement("li")
+    listItem.innerHTML = `<button>Complete</button>${newItem}<button onclick='removeItem(this)'>Remove</button>`
+    listItem.setAttribute('id', `list_item${i}`)
+    i++
+    incomplete.appendChild(listItem)
+}
 
-let newItem = document.getElementById("newListItem").value
-let listItem = document.createElement("li")
-listItem.innerHTML = `<li><button>Complete</button>${newItem}<button>Remove</button></li>`
-incomplete.appendChild(listItem)
+function removeItem(element) {
+    element.parentElement.remove()
 
-
-
+}
