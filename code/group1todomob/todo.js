863517205512e6b3bf5717557748014e3b3be885
Justin <justin.35f@gmail.com>
2022-03-10 16:28:46 -0800

    more local storage

--- code/group1todomob/todo.js
+++ code/group1todomob/todo.js
@@ -1,15 +1,32 @@
 let completed = document.getElementById('completeList')
 let incomplete = document.getElementById('incompleteList')
 let i = 0
-let itemList = []
+let itemList
 
 
 function loadLocalData() {
     localData = JSON.parse(localStorage.getItem('todoItems'))
-    
-    for (const item in localData) {
-        itemList.push(localData[item])
-        
+    if (localStorage.getItem('todoItems')) {
+        itemList = JSON.parse(localStorage.getItem('todoItems'))
+
+        for (const item in localData) {
+            let listItem = document.createElement("li")
+            listItem.setAttribute('id', `list_item${localData[item][0]}`)
+            if (!localData[item][2])
+            {
+                listItem.innerHTML = `<button onclick='completeItem(this)'>Complete</button>${localData[item][1]}<button onclick='removeItem(this)'>Remove</button>`
+                incomplete.appendChild(listItem)
+            }
+            else
+            {
+                listItem.innerHTML = `<button onclick='completeItem(this) disabled="disabled"'>Complete</button>${localData[item][1]}<button onclick='removeItem(this)'>Remove</button>`
+                listItem.classList.add('marked')
+                completed.appendChild(listItem)
+            }
+        }
+    }
+    else {
+        itemList = []
     }
 }
 
@@ -24,7 +41,7 @@ function addNewitem() {
     i++
     incomplete.appendChild(listItem)
 
-    itemList.push([newItem, false])
+    itemList.push([i, newItem, false])
     localStorage.setItem('todoItems', JSON.stringify(itemList))
 }
 
@@ -36,4 +53,9 @@ function completeItem(element) {
     completed.appendChild(element.parentElement)
     element.disabled = 'disabled'
     element.parentElement.classList.add('marked')
+
+    id = element.parentElement.id.replace('list_item', '')
+    console.log(id)
+    itemList[id][2] = true
+    localStorage.setItem('todoItems', JSON.stringify(itemList))
 }
