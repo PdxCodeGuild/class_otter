4fd80de0b481d9f205eff62f1b1798e532dd3222
Justin <justin.35f@gmail.com>
2022-03-10 17:46:06 -0800

    Completed

--- code/group1todomob/todo.js
+++ code/group1todomob/todo.js
@@ -1,11 +1,17 @@
 let completed = document.getElementById('completeList')
 let incomplete = document.getElementById('incompleteList')
-let i = 0
+let uniqueIDIncrementor = 0
 let itemList
 
 
 function loadLocalData() {
     localData = JSON.parse(localStorage.getItem('todoItems'))
+
+    if (localStorage.getItem('uniqueIDIncrementor'))
+    {
+        uniqueIDIncrementor = parseInt(localStorage.getItem('uniqueIDIncrementor'))
+    }
+
     if (localStorage.getItem('todoItems')) {
         itemList = JSON.parse(localStorage.getItem('todoItems'))
 
@@ -14,12 +20,12 @@ function loadLocalData() {
             listItem.setAttribute('id', `list_item${localData[item][0]}`)
             if (!localData[item][2])
             {
-                listItem.innerHTML = `<button onclick='completeItem(this)'>Complete</button>${localData[item][1]}<button onclick='removeItem(this)'>Remove</button>`
+                listItem.innerHTML = `<a class="btn waves-effect waves-light green" onclick='completeItem(this)'><i class="material-icons left">done</i></a>${localData[item][1]}<a class="btn waves-effect waves-light red" onclick='removeItem(this)'><i class="material-icons left">clear</i></a>`
                 incomplete.appendChild(listItem)
             }
             else
             {
-                listItem.innerHTML = `<button onclick='completeItem(this) disabled="disabled"'>Complete</button>${localData[item][1]}<button onclick='removeItem(this)'>Remove</button>`
+                listItem.innerHTML = `<a class="btn waves-effect waves-light green disabled" onclick='completeItem(this)'><i class="material-icons left">done</i></a>${localData[item][1]}<a class="btn waves-effect waves-light red" onclick='removeItem(this)'><i class="material-icons left">clear</i></a>`
                 listItem.classList.add('marked')
                 completed.appendChild(listItem)
             }
@@ -36,26 +42,43 @@ console.log(localStorage)
 function addNewitem() {
     let newItem = document.getElementById("newListItem").value
     let listItem = document.createElement("li")
-    listItem.innerHTML = `<button onclick='completeItem(this)'>Complete</button>${newItem}<button onclick='removeItem(this)'>Remove</button>`
-    listItem.setAttribute('id', `list_item${i}`)
-    i++
+    listItem.innerHTML = `<a class="btn waves-effect waves-light green" onclick='completeItem(this)'><i class="material-icons left">done</i></a>${newItem}<a class="btn waves-effect waves-light red" onclick='removeItem(this)'><i class="material-icons left">clear</i></a>`
+    listItem.setAttribute('id', `list_item${uniqueIDIncrementor}`)
     incomplete.appendChild(listItem)
 
-    itemList.push([i, newItem, false])
+    itemList.push([uniqueIDIncrementor, newItem, false])
     localStorage.setItem('todoItems', JSON.stringify(itemList))
+
+    uniqueIDIncrementor++
+    localStorage.setItem('uniqueIDIncrementor', `${uniqueIDIncrementor}`)
 }
 
 function removeItem(element) {
+    id = parseInt(element.parentElement.id.replace('list_item', ''))
+    for (let index = 0; index < itemList.length; index++) {
+        if (itemList[index][0] === id) {
+            itemList.splice(index, 1)
+            break
+        }
+    }
+
+    localStorage.setItem('todoItems', JSON.stringify(itemList))
     element.parentElement.remove()
 }
 
 function completeItem(element) {
-    completed.appendChild(element.parentElement)
-    element.disabled = 'disabled'
+    element.classList.add('disabled')
     element.parentElement.classList.add('marked')
+    
+    completed.appendChild(element.parentElement)
 
-    id = element.parentElement.id.replace('list_item', '')
-    console.log(id)
-    itemList[id][2] = true
+    id = parseInt(element.parentElement.id.replace('list_item', ''))
+    for (let index = 0; index < itemList.length; index++) {
+        if (itemList[index][0] === id) {
+            itemList[index][2] = true
+            break
+        }
+    }
+
     localStorage.setItem('todoItems', JSON.stringify(itemList))
 }
