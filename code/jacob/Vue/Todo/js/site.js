387c56237bca3bb6dbbb5b251b32d579cc52526d
Jacob <anduril18@hotmail.com>
2022-03-14 16:36:42 -0700

    Vue

---
+++ code/jacob/Vue/Todo/js/site.js
@@ -0,0 +1,26 @@
+new Vue({
+    el: '#app',
+    data: {
+        message: 'My Todo List',
+        todos: [
+            {id: 1, text: "Walk the dog", completed: false},
+            {id: 2, text: "Wash the car", completed: false}
+        ],
+        newTodos: {id: 3, text: "", completed: false}
+    },
+    
+    methods: {
+        addTodo: function(){
+            this.todos.push({
+                id: this.newTodos.id,
+                text: this.newTodos.text,
+                completed: this.newTodos.completed
+            })
+            this.newTodos.id++
+            this.newTodos.text=""
+        },
+        removeTodo: function(todos){
+            this.todos.splice(this.todos.indexOf(todos), 1)
+        }
+    }
+})
