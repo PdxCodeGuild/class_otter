cbb2a6a94f9a5b8c986d405287bdca8d287e99c0
Sana Rahmani <sanarahmani@live.com>
2022-03-15 14:13:21 -0500

    pr

--- code/sana/django/urlshortenerlab/urlsproject/urlsapp/models.py
+++ code/sana/django/urlshortenerlab/urlsproject/urlsapp/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Link(models.Model):
     ulink = models.CharField(max_length=1000)
     slink = models.CharField(max_length=10)
+    
+    def __str__(self) :
+        return self.name
--- code/sana/django/urlshortenerlab/urlsproject/urlsapp/views.py
+++ code/sana/django/urlshortenerlab/urlsproject/urlsapp/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+import uuid
+from .models import Link
+
+
+archives = {}
 
 def index(request):
     # return HttpResponse('page')
@@ -7,3 +12,25 @@ def index(request):
 
 def home(request):
     return HttpResponse('home')
+
+def database(inputurl, shorturl):
+    archives.update({ f"{shorturl}":f"{inputurl}"})
+
+def process(request): 
+    if request.method == 'POST':
+        input_url = request.POST['link']
+        unique_url = str(uuid.uuid4())[:7]
+        new_url = Link(ulink=input_url, slink=unique_url)
+        database(input_url, unique_url)
+        new_url.save()
+        return HttpResponse(unique_url, input_url)
+
+  
+def send(request):
+    if request.method == 'POST':
+        request_url = request.POST['input']
+        if request_url in archives:
+            accepted_url = archives[request_url]
+            return HttpResponseRedirect(accepted_url)
+        else:
+            return redirect('/')
