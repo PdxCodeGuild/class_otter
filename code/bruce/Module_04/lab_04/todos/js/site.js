07fb12b352d92735c1f2a75b46bdf7b4006413b8
Bruce Stull <bruce.stull@gmail.com>
2022-03-15 16:38:13 -0400

    Lab 4: Vue ToDos - In progress - Checkbox list completed. Need to join the button-operated list into one list with completed items on bottom.

--- code/bruce/Module_04/lab_04/todos/js/site.js
+++ code/bruce/Module_04/lab_04/todos/js/site.js
@@ -37,10 +37,17 @@ const app1 = new Vue({
         newText: '',
         nextId: 5,
         todos: [
-            {text: "Get fuzzy!", isCompleted: false, id: 1},
-            {text: "Scrub him!", isCompleted: false, id: 2},
-            {text: "Make sure they're fed!", isCompleted: false, id: 3},
-            {text: "Give em a comfy bed.", isCompleted: false, id: 4},
+            { text: "Scrub the kittens", isCompleted: false, id: 5},
+            { text: "phil fude bole", isCompleted: false, id: 6},
+            { text: "Watch kittens play", isCompleted: false, id: 7},
+            { text: "Scrub Bunbun", isCompleted: false, id: 8},
+            { text: "Pet Bunbun", isCompleted: true, id: 9},
+            { text: "Make sure kittens don't starve", isCompleted: true, id: 10},
+            { text: "Video tape the kittens", isCompleted: true, id: 11},
+            { text: "Get fuzzy!", isCompleted: false, id: 1},
+            { text: "Scrub him!", isCompleted: false, id: 2},
+            { text: "Make sure they're fed!", isCompleted: false, id: 3},
+            { text: "Give em a comfy bed.", isCompleted: false, id: 4},
         ],
     },
     methods: {
@@ -78,16 +85,39 @@ const app1 = new Vue({
         },
 
         addTodo: function(newText) {
-            // Add 'newTodoText' to 'uncompletedTodos'.
             console.log(`Adding todo: ${newText}`)
-            // This line seems to add a 'key' to the 'todos'?
-            this.todos.push({text: newText, isCompleted: false})
+            // this.todos.length()
+            // Add 'newText' to 'todos'.
+            this.todos.push({text: newText, isCompleted: false, id: this.nextId})
+            console.log(`Next ID: ${this.nextId}`)
+            // incrementNextId()
+            this.nextId++
+            console.log(`Next ID: ${this.nextId}`)
         },
         deleteTodo: function(todo) {
+            console.log(`ToDo: ${todo}`)
+            console.log(`ToDo Is Completed: ${todo.isCompleted}`)
+            console.log(`ToDo ID: ${todo.id}`)
             console.log(`Deleting: ${todo.text}`)
             // Need to figure out how to delete the item with specific id.
-            this.todos.splice(index, 1)            
-        }
+            // We have an array. What are methods to modify arrays?
+            // 
+        },
     },
+    computed: {
+        // a computed getter
+        incompleteTodos: function () {
+            // Return list of todos where isCompleted == true.
+            return this.todos.filter(function(todo) {
+                return todo.isCompleted === false
+            })
+        },
+        completeTodos: function () {
+            // Return list of todos where isCompleted == true.
+            return this.todos.filter(function(todo) {
+                return todo.isCompleted === true
+            })
+        },
+      },
 })
 
