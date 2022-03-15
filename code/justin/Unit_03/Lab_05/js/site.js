fed1171d5ff9f17f8ec083f502960da0928c62bb
Justin <justin.35f@gmail.com>
2022-03-15 14:56:14 -0700

    Working on basic functionality and presentation

--- code/justin/Unit_03/Lab_05/js/site.js
+++ code/justin/Unit_03/Lab_05/js/site.js
@@ -1,6 +1,44 @@
-new Vue({
-    el: '#app',
+var addItemApp = new Vue({
+    el: '#addItemApp',
     data: {
-        message: 'Hello world!'
+        newItemText: ''
+    }
+})
+
+Vue.component('todo-item', {
+    props: ['item'],
+    template: '<li><button>Complete</button>{{ item.text }}<button>Remove</button></li>'
+})
+
+var todoListApp = new Vue({
+    el: '#todoListApp',
+    data: {
+        todoList: [
+            { id: 0, text: 'Learn JavaScript', isCompleted: false },
+            { id: 1, text: 'Learn Vue', isCompleted: false },
+            { id: 2, text: 'Build something awesome', isCompleted: false }
+          ]
+    },
+    computed: {
+        incompleteItems: function() {
+            incompleteList = [];
+            for (let i = 0; i < this.todoList.length; i++) {
+                if (!this.todoList[i].isCompleted) {
+                    incompleteList.push(this.todoList[i]);
+                }
+            }
+
+            return incompleteList;
+        },
+        completeItems: function() {
+            completeList = [];
+            for (let i = 0; i < this.todoList.length; i++) {
+                if (this.todoList[i].isCompleted) {
+                    completeList.push(this.todoList[i]);
+                }
+            }
+
+            return completeList;
+        }
     }
 })
