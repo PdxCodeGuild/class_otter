38019ae06c178aa1c79ef0700cd492b78a72e030
Justin <justin.35f@gmail.com>
2022-03-15 15:08:27 -0700

    Add item functionality

--- code/justin/Unit_03/Lab_05/js/site.js
+++ code/justin/Unit_03/Lab_05/js/site.js
@@ -2,21 +2,33 @@ var addItemApp = new Vue({
     el: '#addItemApp',
     data: {
         newItemText: ''
+    },
+    methods: {
+        addNewItem: function() {
+            todoListApp.todoList.push({id: GetTodoID(), text: addItemApp.newItemText, isCompleted: false});
+        }
     }
 })
 
 Vue.component('todo-item', {
     props: ['item'],
-    template: '<li><button>Complete</button>{{ item.text }}<button>Remove</button></li>'
+    template: '<li><button v-on:click="item.isCompleted=true">Complete</button>{{ item.text }}<button>Remove</button></li>'
 })
+
+let idIncrementor = 0;
+function GetTodoID() {
+    let nextID = idIncrementor;
+    idIncrementor++;
+    return nextID;
+}
 
 var todoListApp = new Vue({
     el: '#todoListApp',
     data: {
         todoList: [
-            { id: 0, text: 'Learn JavaScript', isCompleted: false },
-            { id: 1, text: 'Learn Vue', isCompleted: false },
-            { id: 2, text: 'Build something awesome', isCompleted: false }
+            { id: GetTodoID(), text: 'Learn JavaScript', isCompleted: false },
+            { id: GetTodoID(), text: 'Learn Vue', isCompleted: false },
+            { id: GetTodoID(), text: 'Build something awesome', isCompleted: false }
           ]
     },
     computed: {
