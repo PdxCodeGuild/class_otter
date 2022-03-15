a788cbc33e2178acf0527c2af62d52550452e6c7
Nick Corner <nickcorner421@gmail.com>
2022-03-15 11:30:54 -0500

    django updated need styling

--- code/nick/django_lab03/posts/urls.py
+++ code/nick/django_lab03/posts/urls.py
@@ -5,8 +5,5 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('post_detail/', views.PostList.as_view(), name='post_detail'),
-    # path('post/new/', views.PostCreateView.as_view(), name='new'),
-    # path('post/delete/', views.PostDeleteView.as_view(), name='delete'),
+    path('',views.home,name='home')
 ]
