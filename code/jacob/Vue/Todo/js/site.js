b8fb7552a983b7f7fc11ef5bc4f5b356c18a0925
Jacob <anduril18@hotmail.com>
2022-03-15 16:13:31 -0700

    Vue

--- code/jacob/Vue/Todo/js/site.js
+++ code/jacob/Vue/Todo/js/site.js
@@ -6,7 +6,8 @@ new Vue({
             {id: 1, text: "Walk the dog", completed: false},
             {id: 2, text: "Wash the car", completed: false}
         ],
-        newTodos: {id: 3, text: "", completed: false}
+        newTodos: {id: 3, text: "", completed: false},
+        
     },
     
     methods: {
@@ -21,6 +22,38 @@ new Vue({
         },
         removeTodo: function(todo){
             this.todos.splice(this.todos.indexOf(todo), 1)
+        },
+        completed: function(todo){
+            // todo.completed = !todo.completed     this is the short way to do:
+            if (todo.completed === false){
+                todo.completed = true
+            }
+            else{
+                todo.completed = false
+            }
+        }
+    },
+
+    computed: {
+        incomplete: function(){
+            let undone = []
+            for(let i=0; i<this.todos.length; i++){
+                if (this.todos[i].completed === false){
+                    undone.push(this.todos[i])
+                    
+                }
+            }    
+            return undone    
+        },
+        complete: function(){
+            let done = []
+            for(let i= 0; i<this.todos.length; i++){
+                if (this.todos[i].completed === true){
+                    done.push(this.todos[i])
+                }
+            }
+            return done
         }
     }
+
 })
