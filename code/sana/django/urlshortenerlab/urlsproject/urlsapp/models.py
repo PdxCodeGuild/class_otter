bb16f4ef5a47df858948803c2d70e84758484e5d
Sana Rahmani <sanarahmani@live.com>
2022-03-16 13:37:56 -0500

    pr

--- code/sana/django/urlshortenerlab/urlsproject/urlsapp/models.py
+++ code/sana/django/urlshortenerlab/urlsproject/urlsapp/models.py
@@ -6,4 +6,4 @@ class Link(models.Model):
     slink = models.CharField(max_length=10)
     
     def __str__(self) :
-        return self.name
+        return self.slink
--- code/sana/django/urlshortenerlab/urlsproject/urlsapp/views.py
+++ code/sana/django/urlshortenerlab/urlsproject/urlsapp/views.py
@@ -1,36 +1,40 @@
-from django.http import HttpResponse, HttpResponseRedirect
+
 from django.shortcuts import render, redirect
 import uuid
 from .models import Link
 
 
-archives = {}
+# archives = {}
 
 def index(request):
     # return HttpResponse('page')
     return render(request, 'index.html')
 
-def home(request):
-    return HttpResponse('home')
+# def home(request):
+#     return HttpResponse('home')
 
-def database(inputurl, shorturl):
-    archives.update({ f"{shorturl}":f"{inputurl}"})
+# def database(inputurl, shorturl):
+#     archives.update({ f"{shorturl}":f"{inputurl}"})
 
 def process(request): 
     if request.method == 'POST':
-        input_url = request.POST['link']
+        input_url = request.POST['link']      
         unique_url = str(uuid.uuid4())[:7]
         new_url = Link(ulink=input_url, slink=unique_url)
-        database(input_url, unique_url)
         new_url.save()
-        return HttpResponse(unique_url, input_url)
+    return render(request, 'index.html', {'links' : Link.objects.all()})
 
   
-def send(request):
-    if request.method == 'POST':
-        request_url = request.POST['input']
-        if request_url in archives:
-            accepted_url = archives[request_url]
-            return HttpResponseRedirect(accepted_url)
-        else:
-            return redirect('/')
+# def send(request):
+
+    # if request.method == 'POST':
+    #     request_url = request.POST['input']
+    #     if request_url in archives:
+    #         accepted_url = archives[request_url]
+    #         return HttpResponseRedirect(accepted_url)
+    #     else:
+    #         return redirect('/')
+
+def goto(request, pk):
+    go_url = Link.objects.get(ulink=pk)
+    return redirect('http://'+go_url.ulink)
