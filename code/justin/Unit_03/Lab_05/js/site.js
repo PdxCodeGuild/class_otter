163f778043e3773f01568e9f07448d7c5de0e10e
Justin <justin.35f@gmail.com>
2022-03-16 14:24:37 -0700

    Added local storage support

--- code/justin/Unit_03/Lab_05/js/site.js
+++ code/justin/Unit_03/Lab_05/js/site.js
@@ -1,3 +1,43 @@
+let idIncrementor = 0;
+function GetTodoID() {
+    let nextID = idIncrementor;
+    
+    idIncrementor++;
+    localStorage.setItem('idIncrementor', `${idIncrementor}`);
+
+    return nextID;
+}
+
+function LoadLocalData() {
+    idIncrementor = localStorage.getItem('idIncrementor');
+    idIncrementor = (idIncrementor) ? idIncrementor : 0;
+
+    let localTodoItems = JSON.parse(localStorage.getItem('todoItems'));
+    if (localTodoItems) {
+        for (let i = 0; i < localTodoItems.length; i++) {
+            let todoItem = localTodoItems[i];
+            todoListApp.todoList.push({
+                id: todoItem.id,
+                text: todoItem.text,
+                isCompleted: todoItem.isCompleted
+            });
+        }
+    }
+}
+
+function FindIndexByID(itemList, itemID) {
+    let index = -1;
+    for (let i = 0; i < itemList.length; i++) {
+        if (itemList[i].id == itemID) {
+            return i;
+        }
+    }
+
+    console.log(`No item with id: ${itemID} found in todoList.`);
+    return index;
+}
+
+
 var addItemApp = new Vue({
     el: '#addItemApp',
     data: {
@@ -5,7 +45,12 @@ var addItemApp = new Vue({
     },
     methods: {
         addNewItem: function() {
-            todoListApp.todoList.push({id: GetTodoID(), text: addItemApp.newItemText, isCompleted: false});
+            todoListApp.todoList.push({
+                id: GetTodoID(),
+                text: addItemApp.newItemText,
+                isCompleted: false
+            });
+            localStorage.setItem('todoItems', JSON.stringify(todoListApp.todoList));
         }
     }
 })
@@ -13,17 +58,10 @@ var addItemApp = new Vue({
 Vue.component('todo-item', {
     props: ['item'],
     template: `<li class="row">
-                <a :class='["col s1 waves-effect waves-light btn green", item.isCompleted && "disabled"]' v-on:click="item.isCompleted=true"><i class="material-icons left">done</i></a>
+                <a :class='["col s1 waves-effect waves-light btn green", item.isCompleted && "disabled"]' v-on:click="$emit(\'complete\', item.id)"><i class="material-icons left">done</i></a>
                 <span class="col s10">{{ item.text }}</span>
                 <a class="col s1 waves-effect waves-light btn red" v-on:click="$emit(\'remove\', item.id)"><i class="material-icons left">delete_forever</i></a></li>`
 })
-
-let idIncrementor = 0;
-function GetTodoID() {
-    let nextID = idIncrementor;
-    idIncrementor++;
-    return nextID;
-}
 
 var todoListApp = new Vue({
     el: '#todoListApp',
@@ -31,17 +69,18 @@ var todoListApp = new Vue({
         todoList: []
     },
     methods: {
-        remove: function(itemID) {
-            let index = -1;
-            for (let i = 0; i < this.todoList.length; i++) {
-                if (this.todoList[i].id == itemID) {
-                    index = i;
-                    break;
-                }
+        complete: function(itemID) {
+            let index = FindIndexByID(this.todoList, itemID);
+            if (index != -1) {
+                this.todoList[index].isCompleted = true;
+                localStorage.setItem('todoItems', JSON.stringify(this.todoList));
             }
-
+        },
+        remove: function(itemID) {
+            let index = FindIndexByID(this.todoList, itemID);
             if (index != -1) {
                 this.todoList.splice(index, 1);
+                localStorage.setItem('todoItems', JSON.stringify(this.todoList));
             }
         }
     },
@@ -68,3 +107,8 @@ var todoListApp = new Vue({
         }
     }
 })
+
+
+
+
+LoadLocalData();
