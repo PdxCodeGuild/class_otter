126a61d2948f8d95869ca6232b3d651beca101a0
smadden5 <smadden5@gmail.com>
2022-03-17 18:47:41 -0500

    22-03-17 - Vue-Axios

--- code/scott/JS/labs/Vue/favQs/js/favQs.js
+++ code/scott/JS/labs/Vue/favQs/js/favQs.js
@@ -1,17 +1,67 @@
+// Vue.component('quote-me', {
+//     data: function() {
+//         return {
+//             filter: "",
+//             type: ""
+//         }
+//     },
+//     template: `
+//         <div>
+//         <h3>Enter your search text below..</h3>
+//         <input type="type" placeholder="Enter Search Text Here" @keyup.enter="getQuote" v-model="filter">
+//         <h3>Now select from the options below how you would like to search..</h3>
+//         <select v-model="type">
+//         <option disabled value="">Please select one</option>
+//         <option>author</option>
+//         <option>keyword</option>
+//         <option>tag</option>
+//         </select>
+//         <h3>If everything is correct, click below!</h3>
+//             <button @click="getQuote">Get Quote</button>
+//         </div>
+//         `,
+//     methods: {
+//         getQuote: function() {
+//             this.$emit('quote', {type: this.type, filter: this.filter})
+//             this.filter = ""
+//             this.type = ""
+//         }
+//     }
+// })
+
+
+
+// const App = {
+// 	data() {
+// 		return {
+//             randomQuote: '',
+// 			quotes: [], //array literal
+//             lastPage: true, //for paging
+//             authorPage: 1, // method variable and start page
+//             keywordPage: 1, // method variable and start page
+//             tagPage: 1, // method variable and start page
+//             tag: '', // variable and type for Tag Search
+//             author: '', // variable and type for Author Search
+// 			keyword: '', // variable and type for Keyword
+//             type: '',
+// 		}
+//     },
+
+
 const App = {
-	data() {
-		return {
+    data() {
+        return {
             randomQuote: '',
-			quotes: [],
-            lastPage: true,
-            authorPage: 1,
-            keywordPage: 1,
-            tagPage: 1,
-            tag: '', // User Input for Tag Search
-            author: '', // User Input for Author Search
-			keyword: '', //User Input for Keyword
-            type: '',
-		}
+            quotes: [],  //array literal
+            lastPage: true, //for paging
+            authorPage: 1, // method variable and start page
+            keywordPage: 1, // method variable and start page
+            tagPage: 1, // method variable and start page
+            tag: '', // variable and type for Tag Search
+            author: '', // variable and type for Author Search
+            keyword: '', // variable and type for Keyword
+            type: '', // variable used in assigning
+        }
     },
 
     created() {
@@ -19,6 +69,18 @@ const App = {
     },
 
     methods: {
+
+        // loadRandom25: function() {
+        //     axios({
+        //         url: "https://favqs.com/api/quotes",
+        //         method: "get",
+        //         headers: {
+        //             "Authorization": `Token token="${apiKey}"`
+        //         },
+        //     }).then(response => {
+        //         this.results = response.data
+        //     })
+        // },
 
         getRandomQuote() {            
             axios({
@@ -47,21 +109,78 @@ const App = {
             })
         },
 
-        // getQuoteByTag() {
-            // this.type = 'tag'
-            // axios({
-        // },
+        getQuoteByTag() {
+            this.type = 'tag'
+            axios({
+                method: 'get',
+                url: `https://favqs.com/api/quotes/`,
+                headers: {Accept: 'application/json', Authorization: 'Token token="855df50978dc9afd6bf86579913c9f8b"'},
+                params: { filter: this.tag, type: 'tag', page: 1 }
+            })
+            .then(response => {
+                // console.log(response.data.last_page)
+                this.lastPage = response.data.last_page
+                this.quotes = response.data.quotes
 
-        // getQuoteByAuthor() {
-           
-        // },
+            })
+        },
+        
+        getQuoteByAuthor() {
+            this.type = 'author'
+            axios({
+                method: 'get',
+                url: `https://favqs.com/api/quotes/`,
+                headers: {Accept: 'application/json', Authorization: 'Token token="855df50978dc9afd6bf86579913c9f8b"'},
+                params: { filter: this.author, type: 'author', page: 1 }
+            })
+            .then(response => {
+                // console.log(response.data)
+                this.lastPage = response.data.last_page
+                this.quotes = response.data.quotes
+            })
+        },
 
+                        // Use Template literal to compare type & tag
 
+        nextPage() {
+            let myParams = {}
+            if (this.type === 'tag') {
+                myParams = { filter: this.tag, type: this.type, page: this.tagPage +=1 }
+            }
+
+            else if (this.type === 'author') {
+                myParams = { filter: this.author, type: this.type, page: this.authorPage +=1 }
+            }
+
+            else if (this.type === 'keyword') {
+                myParams = { filter: this.keyword, type: this.type, page: this.keywordPage +=1 }
+            }
+
+                axios({
+                    method: 'get',
+                    url: `https://favqs.com/api/quotes/`,
+                    headers: {Accept: 'application/json', Authorization: 'Token token="855df50978dc9afd6bf86579913c9f8b"'},
+                    params: myParams
+                })
+                .then(response => {
+                    this.quotes = response.data.quotes
+                    this.lastPage = response.data.last_page
+                })
         }
     }
-
+}
 
 Vue.createApp(App).mount('#app')
+
+
+App.component('quote', {
+    props: ['quote'],
+    template: ` 
+        <article>
+            <p>"{{ quote.body }}"" by {{ quote.author }}</p>
+        </article> 
+`
+});
 //  axios({
     // method: 'get',
     // url: 'https://favqs.com/api/qotd',
