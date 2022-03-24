217cf18fb78a800d0d85884892159cc4deaca037
Merritt Lawrenson <merrittlawrenson@gmail.com>
2022-03-24 15:42:11 -0700

    finished DRF/Vue example

--- code/merritt/22-jan/drf-blog-22jan/api/serializers.py
+++ code/merritt/22-jan/drf-blog-22jan/api/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 
 from posts.models import Post
 
-class PostSerializer(serializers.ModelSerializer):
+class NestedPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('id', 'title', 'author', 'created', 'updated', 'body')
+        fields = ('id', 'title', 'created', 'updated', 'body')
+
+class NestedUserSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = get_user_model()
+        fields = ('id', 'username')
+
+class PostSerializer(serializers.ModelSerializer):
+    author_detail = NestedUserSerializer(read_only=True, source='author')
+    class Meta:
+        model = Post
+        fields = ('id', 'title', 'author', 'author_detail', 'created', 'updated', 'body')
+
+class UserSerializer(serializers.ModelSerializer):
+    posts_detail = NestedPostSerializer(many=True, read_only=True, source='posts')
+    class Meta: 
+        model = get_user_model()
+        fields = ('id', 'username', 'posts', 'posts_detail')
