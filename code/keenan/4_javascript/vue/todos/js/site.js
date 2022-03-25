2cbfb1d07f589afb704a9e6f8080039edfc7bda5
ktabusa <ktabusa@gmail.com>
2022-03-25 16:32:41 -0700

    minor formatting and documentation updates to old files

--- code/keenan/4_javascript/vue/todos/js/site.js
+++ code/keenan/4_javascript/vue/todos/js/site.js
@@ -25,11 +25,3 @@ const vm = new Vue({
         },
     }
 })
-
-
-// Your Vue app will need to do a few things:
-
-// Store an array of objects (the todos themselves)
-// List each todo
-// Allow the user to add and remove todos
-// Allow a user to toggle if a task is complete or not
