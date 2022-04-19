4cc5753d2c46f7a17eea5085451e6e356cdf52eb
Justin <justin.35f@gmail.com>
2022-04-19 14:57:27 -0700

    Part 4

--- code/justin/Unit_03_Django/Lab_Pokedex/pokedex-main/apis/views.py
+++ code/justin/Unit_03_Django/Lab_Pokedex/pokedex-main/apis/views.py
@@ -1,3 +1,5 @@
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework import viewsets
 from pokemon import models as pokemonModels
 from users import models as userModels
@@ -7,6 +9,28 @@ from .serializers import PokemonSerializer, TypeSerializer, CustomUserSerializer
 class PokemonViewSet(viewsets.ModelViewSet):
     queryset = pokemonModels.Pokemon.objects.all()
     serializer_class = PokemonSerializer
+
+    @action(detail=True, methods=['put'])
+    def capture(self, request, pk=None):
+        caught_by_id = request.data['userID']
+        pokemon = self.get_object()
+
+        pokemon.caught_by.add(caught_by_id)
+        pokemon.save()
+        responseObject = {'caught_by': f'{pokemon.caught_by}'}
+
+        return Response(responseObject)
+
+    @action(detail=True, methods=['put'])
+    def release(self, request, pk=None):
+        caught_by_id = request.data['userID']
+        pokemon = self.get_object()
+        
+        pokemon.caught_by.remove(caught_by_id)
+        pokemon.save()
+        responseObject = {'caught_by': f'{pokemon.caught_by}'}
+
+        return Response(responseObject)
 
 class TypeViewSet(viewsets.ModelViewSet):
     queryset = pokemonModels.Type.objects.all()
--- code/justin/Unit_03_Django/Lab_Pokedex/pokedex-main/static/js/main.js
+++ code/justin/Unit_03_Django/Lab_Pokedex/pokedex-main/static/js/main.js
@@ -1,3 +1,33 @@
+function FindIndexByID(itemList, itemID) {
+    for (let i = 0; i < itemList.length; i++) {
+        if (itemList[i].id == itemID) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+function FindIndexByAttribute(itemList, attributeName, itemAttribute) {
+    for (let i = 0; i < itemList.length; i++) {
+        if (itemList[i][attributeName] == itemAttribute) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+function FindIndexByValue(itemList, itemValue) {
+    for (let i = 0; i < itemList.length; i++) {
+        if (itemList[i] == itemValue) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
 function GetCsrfToken() {
     let csrf_element = document.querySelector('[name=csrfmiddlewaretoken]');
     let csrf_token = 'TokenNotFound';
@@ -6,6 +36,10 @@ function GetCsrfToken() {
     }
     return csrf_token;
 }
+
+
+
+
 const csrftoken = GetCsrfToken();
 
 M.AutoInit();
