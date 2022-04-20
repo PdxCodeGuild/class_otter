3679981be0c9933a14c9bc807a050a74c3ffe35c
ktabusa <ktabusa@gmail.com>
2022-04-20 14:19:35 -0700

    basic Tic Tac Toe Player and Board functionality

--- code/keenan/1_python/week3/lab13.py
+++ code/keenan/1_python/week3/lab13.py
@@ -5,96 +5,84 @@
 # Tic Tac Toe is a game. Players take turns placing tokens (a 'O' or 'X') into a 3x3 grid. Whoever gets three in a row first wins.
 # You will write a Player class and Game class to model Tic Tac Toe, and a function main that models gameplay taking in user inputs through REPL.
 
-
-
-
 # The Player class has the following properties:
 # name = player name
 # token = 'X' or 'O'
+
 class Player:
     def __init__(self, name, token):
         self.name = name
         self.token = token
-        # can i put a get input string in the class def?
-        # self.name = input('What is your player name? ')
-        # self.Token = input('Will you play as "X" or "O"? ')
+
+
+# The Game class has the following methods:
+
 
 class Game:
     def __init__(self):
-        # the locations will be described as 'TL' (top left), 'TC' (top center), 'ML' (mid left), 'C' (center), 'BL' (bottom left) etc.
+        # the locations will be described as '1' (top left), '2' (top center), '3' (top right), '4' (mid left), '5' (center), '6' (mid right), '7' (bottom left) etc.
         self.board = {
-            "TL": " ", "TC": " ", "TR": " ",
-            "ML": " ", "C": " ", "MR": " ",
-            "BL": " ", "BC": " ", "BR": " ",
+            "1": " ", "2": " ", "3": " ",
+            "4": " ", "5": " ", "6": " ",
+            "7": " ", "8": " ", "9": " ",
         }
+
 
 # __repr__() Returns a pretty string representation of the game board
 # >>> print(board)
-# X| | 
-# O|X|O
-#  | | 
     def __repr__(self):
-        print(self.board["TL"] + "|" + self.board["TC"] + "|" + self.board["TR"])
-        print(self.board["ML"] + "|" + self.board["C"] + "|" + self.board["MR"])
-        print(self.board["BL"] + "|" + self.board["BC"] + "|" + self.board["BR"])
+        print(self.board["1"] + "|" + self.board["2"] + "|" + self.board["3"])
+        print(self.board["4"] + "|" + self.board["5"] + "|" + self.board["6"])
+        print(self.board["7"] + "|" + self.board["8"] + "|" + self.board["9"])   
     
+
+# move(x, y, player) Place a player's token character string at a given coordinate (top-left is 0, 0), x is horizontal position, y is vertical position.
+# >>> board.move(2, 1, player_1)
     def move(self, location, token):
         if self.board[location] != " ":
             return 'invalid move'
         else:
-            self.board = token  
+            self.board = player.token
 
-# need to check on the tuple/dictionary referencing to move / print board.
+# calc_winner() What token character string has won or None if no one has.
+    def calc_winner():
+        pass
+
+# is_full() Returns true if the game board is full.
+    def is_full():
+        pass
+
+# is_game_over() Returns true if the game board is full or a player has won.
+    def is_game_over():
+        pass
+
+# is_full() Returns true if the game board is full.
+    def is_full():
+        pass
 
 #start by getting the game to print the board.
 # continue by adding the basic name functionality
 # add moves
 # add checks
 
+def main():
+    game = Game()
 
-# def game():
-#     for i in range(10):
-#         __repr__(self)
-#         print(
+    player1 = input(f"Player one please enter your name: ")
+    player2 = input(f"Player two please enter your name: ")
+    
+    player1_token = input(f"Select 'X' or 'O' as your token: ").upper()
+    
+    if player1_token != "X" and player1_token != "O":
+        print("that is not a valid token.")
+    elif player1_token == "X":
+            player2_token = "O"
+    else:
+            player2_token = "X"
 
+    player1 = Player(player1, player1_token)
+    player2 = Player(player2, player2_token)
 
-# The Game class has the following properties:
-# board = your representation of the board
-# You can represent the board however you like, such as a 2D list, tuples, or dictionary.   
+    game.__repr__()
 
-# The Game class has the following methods:
-# __repr__() Returns a pretty string representation of the game board
-# >>> print(board)
-# X| | 
-# O|X|O
-#  | | 
-
-# move(x, y, player) Place a player's token character string at a given coordinate (top-left is 0, 0), x is horizontal position, y is vertical position.
-# >>> board.move(2, 1, player_1)
-#  | | 
-#  | |X
-#  | | 
-# calc_winner() What token character string has won or None if no one has.
-# X| | 
-# O|X|O
-#  | |X
-# >>> board.calc_winner()
-# X
-# is_full() Returns true if the game board is full.
-# X|O|X
-# X|X|O
-# O|O|X
-# >>> board.is_full()
-# True
-# is_game_over() Returns true if the game board is full or a player has won.
-# X|O|X
-# X|X|O
-# O|O|X
-# >>> board.is_game_over()
-# True
-
-# X|O|
-#  | |X
-#  | |
-# >>> board.is_game_over()
-# False
+main()
